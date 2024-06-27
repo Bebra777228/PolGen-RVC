8c5e4d913708a2a4e8b5358789129e7dd064c3eb
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-06-28 04:02:59 +0500

    Update main.py

--- _CoverGen/src/main.py
+++ _CoverGen/src/main.py
@@ -136,7 +136,7 @@ def convert_to_stereo(audio_path):
 
 
 def pitch_shift(audio_path, pitch_change):
-    output_path = f'{os.path.splitext(audio_path)[0]}_p{pitch_change}.wav'
+    output_path = f'{os.path.splitext(audio_path)[0]}_overall_pitch.wav'
     if not os.path.exists(output_path):
         y, sr = sf.read(audio_path)
         tfm = sox.Transformer()
@@ -306,43 +306,44 @@ def song_cover_pipeline(song_input, voice_model, pitch_change, keep_files,
                 orig_song_path, instrumentals_path, main_vocals_dereverb_path, backup_vocals_path = paths
 
         # pitch_change = pitch_change * 12 + pitch_change_all
-        ai_vocals_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]}_{voice_model}_p{pitch_change}_i{index_rate}_fr{filter_radius}_rms{rms_mix_rate}_pro{protect}_{f0_method}{"" if f0_method != "mangio-crepe" else f"_{crepe_hop_length}"}.wav')
+        ai_vocals_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]}_{voice_model}_converted_voice.wav')
         ai_cover_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]} ({voice_model} Ver).{output_format}')
 
-        # Remove old files
-        if os.path.exists(ai_vocals_path):
-            os.remove(ai_vocals_path)
         if os.path.exists(ai_cover_path):
             os.remove(ai_cover_path)
+        if os.path.exists(ai_vocals_path):
+            os.remove(ai_vocals_path)
 
         if not os.path.exists(ai_vocals_path):
             display_progress('[~] Преобразование голоса с помощью RVC...', 0.5, is_webui, progress)
             voice_change(voice_model, main_vocals_dereverb_path, ai_vocals_path, pitch_change, f0_method, index_rate, filter_radius, rms_mix_rate, protect, crepe_hop_length, is_webui)
 
         display_progress('[~] Применение аудиоэффектов к вокалу...', 0.8, is_webui, progress)
-        ai_vocals_mixed_path = add_audio_effects(ai_vocals_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping,
-                                                  low_shelf_gain, high_shelf_gain, limiter_threshold,
-                                                  compressor_ratio, compressor_threshold, delay_time, delay_feedback,
-                                                  noise_gate_threshold, noise_gate_ratio, noise_gate_attack, noise_gate_release)
-
-        if pitch_change != 0:
-            display_progress('[~] Изменение тона вокала...', 0.85, is_webui, progress)
-            ai_vocals_path = pitch_shift(ai_vocals_path, pitch_change)
+        ai_vocals_mixed_path = add_audio_effects(ai_vocals_path, reverb_rm_size, reverb_wet, reverb_dry,
+                                                reverb_damping, reverb_width, low_shelf_gain, high_shelf_gain, 
+                                                limiter_threshold, compressor_ratio, compressor_threshold, 
+                                                delay_time, delay_feedback, noise_gate_threshold, noise_gate_ratio, 
+                                                noise_gate_attack, noise_gate_release)
 
         if pitch_change_all != 0:
             display_progress('[~] Общее изменение тона...', 0.85, is_webui, progress)
-            backup_vocals_path = pitch_shift(backup_vocals_path, pitch_change_all)
-            instrumentals_path = pitch_shift(instrumentals_path, pitch_change_all)
+            backup_vocals_path_ap = pitch_shift(backup_vocals_path, pitch_change_all)
+            instrumentals_path_ap = pitch_shift(instrumentals_path, pitch_change_all)
 
             if pitch_change_ai_vocals:
                 ai_vocals_mixed_path = pitch_shift(ai_vocals_mixed_path, pitch_change_all)
 
-        display_progress('[~] Объединение AI-вокала и инструментальной части...', 0.9, is_webui, progress)
-        combine_audio([ai_vocals_mixed_path, backup_vocals_path, instrumentals_path], ai_cover_path, main_gain, backup_gain, inst_gain, output_format)
+            display_progress('[~] Объединение AI-вокала и инструментальной части...', 0.9, is_webui, progress)
+            combine_audio([ai_vocals_mixed_path, backup_vocals_path_ap, instrumentals_path_ap], ai_cover_path, main_gain, backup_gain, inst_gain, output_format)
+        else:
+            display_progress('[~] Объединение AI-вокала и инструментальной части...', 0.9, is_webui, progress)
+            combine_audio([ai_vocals_mixed_path, backup_vocals_path, instrumentals_path], ai_cover_path, main_gain, backup_gain, inst_gain, output_format)
 
         if not keep_files:
             display_progress('[~] Удаление промежуточных аудиофайлов...', 0.95, is_webui, progress)
             intermediate_files = [vocals_path, main_vocals_path, ai_vocals_mixed_path]
+            if pitch_change_all != 0:
+                intermediate_files += [backup_vocals_path_ap, instrumentals_path_ap]
             for file in intermediate_files:
                 if file and os.path.exists(file):
                     os.remove(file)
