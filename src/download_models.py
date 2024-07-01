3cb75726e75d83e824430f0ef4c985665ee00f36
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-01 18:31:48 +0500

    v0.5

--- src/download_models.py
+++ src/download_models.py
@@ -2,7 +2,8 @@ from pathlib import Path
 import requests
 
 MDX_DOWNLOAD_LINK = 'https://github.com/TRvlvr/model_repo/releases/download/all_public_uvr_models/'
-RVC_DOWNLOAD_LINK = 'https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/'
+RVC_other_DOWNLOAD_LINK = 'https://huggingface.co/Politrees/all_RVC-pretrained_and_other/resolve/main/other/'
+RVC_hubert_DOWNLOAD_LINK = 'https://huggingface.co/Politrees/all_RVC-pretrained_and_other/resolve/main/HuBERTs/'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 mdxnet_models_dir = BASE_DIR / 'mdxnet_models'
@@ -18,14 +19,19 @@ def dl_model(link, model_name, dir_name):
 
 
 if __name__ == '__main__':
-    mdx_model_names = ['UVR-MDX-NET-Voc_FT.onnx', 'UVR_MDXNET_KARA_2.onnx', 'Reverb_HQ_By_FoxJoy.onnx']
+    mdx_model_names = ['Kim_Vocal_2.onnx', 'UVR_MDXNET_KARA_2.onnx', 'Reverb_HQ_By_FoxJoy.onnx']
     for model in mdx_model_names:
         print(f'Downloading {model}...')
         dl_model(MDX_DOWNLOAD_LINK, model, mdxnet_models_dir)
 
-    rvc_model_names = ['hubert_base.pt', 'rmvpe.pt']
-    for model in rvc_model_names:
+    rvc_other_names = ['rmvpe.pt', 'fcpe.pt']
+    for model in rvc_other_names:
         print(f'Downloading {model}...')
-        dl_model(RVC_DOWNLOAD_LINK, model, rvc_models_dir)
+        dl_model(RVC_other_DOWNLOAD_LINK, model, rvc_models_dir)
+
+    rvc_hubert_names = ['hubert_base.pt']
+    for model in rvc_hubert_names:
+        print(f'Downloading {model}...')
+        dl_model(RVC_hubert_DOWNLOAD_LINK, model, rvc_models_dir)
 
     print('All models downloaded!')
