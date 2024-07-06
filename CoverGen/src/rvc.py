75e0ce39bde258083a2203165df2942137f6d2eb
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-06 12:36:16 +0500

    Update rvc.py

--- CoverGen/src/rvc.py
+++ CoverGen/src/rvc.py
@@ -160,8 +160,8 @@ def rvc_infer(
     crepe_hop_length,
     vc,
     hubert_model,
-    f0_min,
-    f0_max
+    f0_min=50,
+    f0_max=1100
 ):
     audio = load_audio(input_path, 16000)
     times = [0, 0, 0]
@@ -185,8 +185,8 @@ def rvc_infer(
         version,
         protect,
         crepe_hop_length,
-        f0_min,
-        f0_max,
-        f0_file=None
+        f0_file=None,
+        f0_min=f0_min,
+        f0_max=f0_max
     )
     wavfile.write(output_path, tgt_sr, audio_opt)
