c758ea284e85ee2a5cbb398dc403bf6e85cdb5c5
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-08 16:25:25 +0500

    Update main.py

--- CoverGen/src/main.py
+++ CoverGen/src/main.py
@@ -153,11 +153,11 @@ def preprocess_song(song_input, mdx_model_params, song_id, is_webui, input_type,
     display_progress('[~] Отделение вокала от инструментала...', 0.1, is_webui, progress)
     vocals_path, instrumentals_path = run_mdx(mdx_model_params, song_output_dir, os.path.join(mdxnet_models_dir, 'Kim_Vocal_2.onnx'), orig_song_path, denoise=True, keep_orig=keep_orig)
 
-    display_progress('[~] Разделение основного вокала и бэк-вокала...', 0.2, is_webui, progress)
-    backup_vocals_path, main_vocals_path = run_mdx(mdx_model_params, song_output_dir, os.path.join(mdxnet_models_dir, 'UVR_MDXNET_KARA_2.onnx'), vocals_path, suffix='Backup', invert_suffix='Main', denoise=True)
-
     display_progress('[~] Применение DeReverb к вокалу...', 0.3, is_webui, progress)
-    _, main_vocals_dereverb_path = run_mdx(mdx_model_params, song_output_dir, os.path.join(mdxnet_models_dir, 'Reverb_HQ_By_FoxJoy.onnx'), main_vocals_path, invert_suffix='DeReverb', exclude_main=True, denoise=True)
+    _, main_vocals_dereverb_path = run_mdx(mdx_model_params, song_output_dir, os.path.join(mdxnet_models_dir, 'Reverb_HQ_By_FoxJoy.onnx'), vocals_path, invert_suffix='DeReverb', exclude_main=True, denoise=True)
+    
+    display_progress('[~] Разделение основного вокала и бэк-вокала...', 0.2, is_webui, progress)
+    backup_vocals_path, main_vocals_path = run_mdx(mdx_model_params, song_output_dir, os.path.join(mdxnet_models_dir, 'UVR_MDXNET_KARA_2.onnx'), main_vocals_dereverb_path, suffix='Backup', invert_suffix='Main', denoise=True)
 
     return orig_song_path, vocals_path, instrumentals_path, main_vocals_path, backup_vocals_path, main_vocals_dereverb_path
 
@@ -257,28 +257,41 @@ def song_cover_pipeline(song_input, voice_model, pitch_change, keep_files, is_we
             else:
                 orig_song_path, instrumentals_path, main_vocals_dereverb_path, backup_vocals_path = paths
 
-        ai_vocals_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]}_{voice_model}_converted_voice.wav')
+        ai_vocals_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]}_{voice_model}_converted_voice.{output_format}')
+        ai_backup_vocals_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]}_{voice_model}_converted_backup_voice.wav')
         ai_cover_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]} ({voice_model} Ver).{output_format}')
 
         if os.path.exists(ai_vocals_path):
             os.remove(ai_vocals_path)
+        if os.path.exists(ai_backup_vocals_path):
+            os.remove(ai_backup_vocals_path)
         if os.path.exists(ai_cover_path):
             os.remove(ai_cover_path)
 
         if not os.path.exists(ai_vocals_path):
             display_progress('[~] Преобразование вокала...', 0.5, is_webui, progress)
-            voice_change(voice_model, main_vocals_dereverb_path, ai_vocals_path, pitch_change, f0_method, index_rate, 
-                         filter_radius, rms_mix_rate, protect, crepe_hop_length, is_webui)
+            voice_change(voice_model, main_vocals_dereverb_path, ai_vocals_path, pitch_change, f0_method,
+                         index_rate, filter_radius, rms_mix_rate, protect, crepe_hop_length, is_webui)
+        
+        if not os.path.exists(ai_backup_vocals_path):
+            display_progress('[~] Converting backup_voice using RVC...', 0.6, is_webui, progress)
+            voice_change(voice_model, backup_vocals_path, ai_backup_vocals_path, pitch_change, f0_method,
+                         index_rate, filter_radius, rms_mix_rate, protect, crepe_hop_length, is_webui)
 
         display_progress('[~] Применение аудиоэффектов к вокалу...', 0.8, is_webui, progress)
         ai_vocals_mixed_path = add_audio_effects(ai_vocals_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping, reverb_width, low_shelf_gain, high_shelf_gain, limiter_threshold, 
                                                  compressor_ratio, compressor_threshold, delay_time, delay_feedback, noise_gate_threshold, noise_gate_ratio, noise_gate_attack, 
                                                  noise_gate_release, drive_db, chorus_rate_hz, chorus_depth, chorus_centre_delay_ms, chorus_feedback, chorus_mix, clipping_threshold)
-
+        
+        ai_backup_vocals_mixed_path = add_audio_effects(ai_backup_vocals_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping, reverb_width, low_shelf_gain, high_shelf_gain, limiter_threshold, 
+                                                 compressor_ratio, compressor_threshold, delay_time, delay_feedback, noise_gate_threshold, noise_gate_ratio, noise_gate_attack, 
+                                                 noise_gate_release, drive_db, chorus_rate_hz, chorus_depth, chorus_centre_delay_ms, chorus_feedback, chorus_mix, clipping_threshold)
+        
         display_progress('[~] Объединение AI-вокала и инструментальной части...', 0.9, is_webui, progress)
         combine_audio([ai_vocals_mixed_path, backup_vocals_path, instrumentals_path], ai_cover_path, main_gain, backup_gain, inst_gain, output_format)
+        combine_audio([ai_vocals_mixed_path, ai_backup_vocals_mixed_path, instrumentals_path], ai_cover_path, main_gain, backup_gain, inst_gain)
 
-        intermediate_files = [vocals_path, main_vocals_path, ai_vocals_mixed_path]
+        intermediate_files = [vocals_path, main_vocals_path, ai_vocals_mixed_path, ai_backup_vocals_mixed_path]
 
         if not keep_files:
             display_progress('[~] Удаление промежуточных аудиофайлов...', 0.95, is_webui, progress)
@@ -286,7 +299,7 @@ def song_cover_pipeline(song_input, voice_model, pitch_change, keep_files, is_we
                 if file and os.path.exists(file):
                     os.remove(file)
 
-        return [ai_cover_path, ai_vocals_path, main_vocals_dereverb_path, backup_vocals_path, instrumentals_path]
+        return [ai_cover_path, ai_backup_vocals_path, ai_vocals_path, main_vocals_dereverb_path, backup_vocals_path, instrumentals_path]
 
 
     except Exception as e:
