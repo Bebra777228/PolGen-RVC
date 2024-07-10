906b0028caa200b275ef6d2d5d7a18d7e98c8e2f
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-10 17:33:15 +0500

    Update main.py

--- CoverGenLite/src/main.py
+++ CoverGenLite/src/main.py
@@ -43,22 +43,13 @@ def get_rvc_model(voice_model, is_webui):
 
 def get_audio_paths(song_dir):
     orig_song_path = None
-    instrumentals_path = None
     main_vocals_dereverb_path = None
-    backup_vocals_path = None
 
     for file in os.listdir(song_dir):
-        if file.endswith('_Instrumental.wav'):
-            instrumentals_path = os.path.join(song_dir, file)
-            orig_song_path = instrumentals_path.replace('_Instrumental', '')
-
-        elif file.endswith('_Vocals_Main_DeReverb.wav'):
+        if file.endswith('_Vocals_Main_DeReverb.wav'):
             main_vocals_dereverb_path = os.path.join(song_dir, file)
 
-        elif file.endswith('_Vocals_Backup.wav'):
-            backup_vocals_path = os.path.join(song_dir, file)
-
-    return orig_song_path, instrumentals_path, main_vocals_dereverb_path, backup_vocals_path
+    return orig_song_path, main_vocals_dereverb_path
 
 def convert_to_stereo(audio_path):
     wave, sr = librosa.load(audio_path, mono=False, sr=44100)
@@ -96,13 +87,7 @@ def voice_change(voice_model, vocals_path, output_path, pitch_change, f0_method,
     del hubert_model, cpt
     gc.collect()
 
-def combine_audio(audio_paths, output_path, main_gain, backup_gain, inst_gain, output_format):
-    main_vocal_audio = AudioSegment.from_wav(audio_paths[0]) - 4 + main_gain
-    backup_vocal_audio = AudioSegment.from_wav(audio_paths[1]) - 6 + backup_gain
-    instrumental_audio = AudioSegment.from_wav(audio_paths[2]) - 7 + inst_gain
-    main_vocal_audio.overlay(backup_vocal_audio).overlay(instrumental_audio).export(output_path, format=output_format)
-
-def song_cover_pipeline(song_input, voice_model, pitch_change, is_webui=0, main_gain=0, backup_gain=0, inst_gain=0, index_rate=0.5, filter_radius=3, rms_mix_rate=0.25, f0_method='rmvpe',
+def song_cover_pipeline(song_input, voice_model, pitch_change, is_webui=0, index_rate=0.5, filter_radius=3, rms_mix_rate=0.25, f0_method='rmvpe',
                         crepe_hop_length=128, protect=0.33, output_format='mp3', progress=gr.Progress()):
 
     try:
@@ -123,37 +108,24 @@ def song_cover_pipeline(song_input, voice_model, pitch_change, is_webui=0, main_
 
         if not os.path.exists(song_dir):
             os.makedirs(song_dir)
-            orig_song_path, instrumentals_path, main_vocals_dereverb_path, backup_vocals_path = get_audio_paths(song_dir)
+            orig_song_path, main_vocals_dereverb_path = get_audio_paths(song_dir)
         else:
             paths = get_audio_paths(song_dir)
 
             if any(path is None for path in paths):
-                orig_song_path, instrumentals_path, main_vocals_dereverb_path, backup_vocals_path = get_audio_paths(song_dir)
+                orig_song_path, main_vocals_dereverb_path = get_audio_paths(song_dir)
             else:
-                orig_song_path, instrumentals_path, main_vocals_dereverb_path, backup_vocals_path = paths
+                orig_song_path, main_vocals_dereverb_path = paths
 
         ai_vocals_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]}_{voice_model}_converted_voice.wav')
         ai_cover_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]} ({voice_model} Ver).{output_format}')
-
-        if os.path.exists(ai_vocals_path):
-            os.remove(ai_vocals_path)
-        if os.path.exists(ai_cover_path):
-            os.remove(ai_cover_path)
 
         if not os.path.exists(ai_vocals_path):
             display_progress('[~] Преобразование вокала...', 0.5, is_webui, progress)
             voice_change(voice_model, main_vocals_dereverb_path, ai_vocals_path, pitch_change, f0_method, index_rate,
                          filter_radius, rms_mix_rate, protect, crepe_hop_length, is_webui)
 
-        display_progress('[~] Объединение AI-вокала и инструментальной части...', 0.9, is_webui, progress)
-        combine_audio([ai_vocals_path, backup_vocals_path, instrumentals_path], ai_cover_path, main_gain, backup_gain, inst_gain, output_format)
-
-        display_progress('[~] Удаление промежуточных аудиофайлов...', 0.95, is_webui, progress)
-        for file in [ai_vocals_path]:
-            if file and os.path.exists(file):
-                os.remove(file)
-
-        return [ai_cover_path, ai_vocals_path, main_vocals_dereverb_path, backup_vocals_path, instrumentals_path]
+        return [ai_cover_path, ai_vocals_path, main_vocals_dereverb_path]
 
     except Exception as e:
         raise_exception(str(e), is_webui)
