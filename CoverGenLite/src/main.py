0420f285a046ff00d2daa258617d21161e4e68e5
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-10 17:38:12 +0500

    Update main.py

--- CoverGenLite/src/main.py
+++ CoverGenLite/src/main.py
@@ -26,30 +26,15 @@ def raise_exception(error_msg, is_webui):
         raise Exception(error_msg)
 
 def get_rvc_model(voice_model, is_webui):
-    rvc_model_filename, rvc_index_filename = None, None
     model_dir = os.path.join(rvc_models_dir, voice_model)
-    for file in os.listdir(model_dir):
-        ext = os.path.splitext(file)[1]
-        if ext == '.pth':
-            rvc_model_filename = file
-        if ext == '.index':
-            rvc_index_filename = file
+    rvc_model_path = next((os.path.join(model_dir, f) for f in os.listdir(model_dir) if f.endswith('.pth')), None)
+    rvc_index_path = next((os.path.join(model_dir, f) for f in os.listdir(model_dir) if f.endswith('.index')), None)
 
-    if rvc_model_filename is None:
+    if rvc_model_path is None:
         error_msg = f'В каталоге {model_dir} отсутствует файл модели.'
         raise_exception(error_msg, is_webui)
 
-    return os.path.join(model_dir, rvc_model_filename), os.path.join(model_dir, rvc_index_filename) if rvc_index_filename else ''
-
-def get_audio_paths(song_dir):
-    orig_song_path = None
-
-    for file in os.listdir(song_dir):
-        if file.endswith('.wav'):
-            orig_song_path = os.path.join(song_dir, file)
-            break
-
-    return orig_song_path
+    return rvc_model_path, rvc_index_path
 
 def convert_to_stereo(audio_path):
     wave, sr = librosa.load(audio_path, mono=False, sr=44100)
@@ -90,37 +75,26 @@ def voice_change(voice_model, vocals_path, output_path, pitch_change, f0_method,
 def song_cover_pipeline(song_input, voice_model, pitch_change, is_webui=0, index_rate=0.5, filter_radius=3, rms_mix_rate=0.25, f0_method='rmvpe',
                         crepe_hop_length=128, protect=0.33, output_format='mp3', progress=gr.Progress()):
 
-    try:
-        if not song_input or not voice_model:
-            raise_exception('Убедитесь, что поле ввода песни и поле модели голоса заполнены.', is_webui)
+    if not song_input or not voice_model:
+        raise_exception('Убедитесь, что поле ввода песни и поле модели голоса заполнены.', is_webui)
 
-        display_progress('[~] Запуск конвейера генерации AI-кавера...', 0, is_webui, progress)
+    display_progress('[~] Запуск конвейера генерации AI-кавера...', 0, is_webui, progress)
 
-        if os.path.exists(song_input):
-            song_id = get_hash(song_input)
-            orig_song_path = song_input
-        else:
-            error_msg = f'{song_input} не существует.'
-            song_id = None
-            raise_exception(error_msg, is_webui)
+    if not os.path.exists(song_input):
+        error_msg = f'{song_input} не существует.'
+        raise_exception(error_msg, is_webui)
 
-        song_dir = os.path.join(output_dir, song_id)
+    song_id = get_hash(song_input)
+    song_dir = os.path.join(output_dir, song_id)
+    os.makedirs(song_dir, exist_ok=True)
 
-        if not os.path.exists(song_dir):
-            os.makedirs(song_dir)
-            orig_song_path = get_audio_paths(song_dir)
-        else:
-            orig_song_path = get_audio_paths(song_dir)
+    orig_song_path = convert_to_stereo(song_input)
+    ai_vocals_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]}_{voice_model}_converted_voice.wav')
+    ai_cover_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]} ({voice_model} Ver).{output_format}')
 
-        ai_vocals_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]}_{voice_model}_converted_voice.wav')
-        ai_cover_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]} ({voice_model} Ver).{output_format}')
+    if not os.path.exists(ai_vocals_path):
+        display_progress('[~] Преобразование вокала...', 0.5, is_webui, progress)
+        voice_change(voice_model, orig_song_path, ai_vocals_path, pitch_change, f0_method, index_rate,
+                     filter_radius, rms_mix_rate, protect, crepe_hop_length, is_webui)
 
-        if not os.path.exists(ai_vocals_path):
-            display_progress('[~] Преобразование вокала...', 0.5, is_webui, progress)
-            voice_change(voice_model, orig_song_path, ai_vocals_path, pitch_change, f0_method, index_rate,
-                         filter_radius, rms_mix_rate, protect, crepe_hop_length, is_webui)
-
-        return [ai_cover_path, ai_vocals_path]
-
-    except Exception as e:
-        raise_exception(str(e), is_webui)
+    return [ai_cover_path, ai_vocals_path]
