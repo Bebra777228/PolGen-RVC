7ebbd6146804f1f548e1bd9661dea533c6c03779
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-12 19:05:51 +0500

    Update audio_effects.py

--- CoverGenLite/src/audio_effects.py
+++ CoverGenLite/src/audio_effects.py
@@ -1,4 +1,8 @@
+import os
+import librosa
 import numpy as np
+import gradio as gr
+import soundfile as sf
 from pedalboard import (
     Pedalboard, Reverb, Compressor, HighpassFilter,
     LowShelfFilter, HighShelfFilter, Limiter, Delay,
@@ -7,19 +11,32 @@ from pedalboard import (
 from pedalboard.io import AudioFile
 from pydub import AudioSegment
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 def display_progress(percent, message, progress=gr.Progress()):
     progress(percent, desc=message)
+
+def combine_audio(vocal_path, instrumental_path, output_path, vocal_gain, instrumental_gain, output_format):
+    vocal_format = vocal_path.split('.')[-1]
+    instrumental_format = instrumental_path.split('.')[-1]
+
+    vocal = AudioSegment.from_file(vocal_path, format=vocal_format)
+    instrumental = AudioSegment.from_file(instrumental_path, format=instrumental_format)
+
+    vocal += vocal_gain
+    instrumental += instrumental_gain
+
+    combined = vocal.overlay(instrumental)
+    combined.export(output_path, format=output_format)
 
 def add_audio_effects(vocal_audio_path, instrumental_audio_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping, reverb_width,
                       low_shelf_gain, high_shelf_gain, limiter_threshold, compressor_ratio, compressor_threshold,
                       delay_time, delay_feedback, noise_gate_threshold, noise_gate_ratio, noise_gate_attack,
                       noise_gate_release, drive_db, chorus_rate_hz, chorus_depth, chorus_centre_delay_ms,
-                      chorus_feedback, chorus_mix, clipping_threshold, output_format, progress=gr.Progress()):
+                      chorus_feedback, chorus_mix, clipping_threshold, output_format, vocal_gain, instrumental_gain, progress=gr.Progress()):
 
     if not vocal_audio_path or not instrumental_audio_path:
         raise ValueError("Оба пути к аудиофайлам должны быть заполнены.")
-                        
-    vocal_output_path = f'{os.path.splitext(vocal_audio_path)[0]}_mixed.wav'
 
     display_progress(0.2, "Применение аудиоэффектов к вокалу...", progress)
     board = Pedalboard(
@@ -38,6 +55,7 @@ def add_audio_effects(vocal_audio_path, instrumental_audio_path, reverb_rm_size,
          ]
     )
 
+    vocal_output_path = f'{os.path.splitext(vocal_audio_path)[0]}_mixed.wav'
     with AudioFile(vocal_audio_path) as f:
         with AudioFile(vocal_output_path, 'w', f.samplerate, 2) as o:
             while f.tell() < f.frames:
@@ -47,16 +65,16 @@ def add_audio_effects(vocal_audio_path, instrumental_audio_path, reverb_rm_size,
                 o.write(effected)
 
     display_progress(0.5, "Объединение вокала и инструментальной части...", progress)
-    vocal_audio = AudioSegment.from_wav(vocal_output_path)
-    instrumental_audio = AudioSegment.from_wav(instrumental_audio_path)
+    output_dir = os.path.join(BASE_DIR, 'processed_output')
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    combined_output_path = os.path.join(output_dir, f'{os.path.splitext(os.path.basename(vocal_audio_path))[0]}_combined.{output_format}')
 
-    combined_audio = vocal_audio.overlay(instrumental_audio)
+    if os.path.exists(combined_output_path):
+        os.remove(combined_output_path)
 
-    display_progress(0.8, "Сохранение объединенного аудиофайла...", progress)
-    combined_output_path = f'{os.path.splitext(vocal_audio_path)[0]}_combined.{output_format}'
-    combined_audio.export(combined_output_path, format=output_format)
+    combine_audio(vocal_output_path, instrumental_audio_path, combined_output_path, vocal_gain, instrumental_gain, output_format)
 
     display_progress(1.0, "Готово!", progress)
-                        
+
     return combined_output_path
-                        
