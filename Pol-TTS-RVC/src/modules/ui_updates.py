22d720de169a16b819f28a0660405e0f4226c59b
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-15 04:00:34 +0500

    Update ui_updates.py

--- Pol-TTS-RVC/src/modules/ui_updates.py
+++ Pol-TTS-RVC/src/modules/ui_updates.py
@@ -13,7 +13,3 @@ def update_f0_method(use_hybrid_methods):
         return gr.update(choices=['hybrid[rmvpe+fcpe]', 'hybrid[rmvpe+crepe]', 'hybrid[crepe+rmvpe]', 'hybrid[crepe+fcpe]', 'hybrid[crepe+rmvpe+fcpe]'], value='hybrid[rmvpe+fcpe]')
     else:
         return gr.update(choices=['rmvpe+', 'fcpe', 'rmvpe', 'mangio-crepe', 'crepe'], value='rmvpe+')
-
-
-def update_voices(selected_language):
-    return gr.update(choices=voices[selected_language])
