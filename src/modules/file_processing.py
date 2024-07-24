39b332141e7ca55222394da3aa48d542e3edaca5
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-24 14:37:06 +0500

    Update file_processing.py

--- src/modules/file_processing.py
+++ src/modules/file_processing.py
@@ -3,3 +3,11 @@ import gradio as gr
 
 def process_file_upload(file):
     return file.name, gr.update(value=file.name)
+
+
+def download_and_save_hubert_model(model_name):
+    try:
+        download_hubert_model(model_name)
+        return f"Модель HuBERT '{model_name}' успешно загружена и сохранена."
+    except Exception as e:
+        return str(e)
