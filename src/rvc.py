6f7301b1cf9b6f15ce9479f67cb022fa53e10b52
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-29 02:11:49 +0500

    Update rvc.py

--- src/rvc.py
+++ src/rvc.py
@@ -6,16 +6,16 @@ import torch
 from fairseq import checkpoint_utils
 from scipy.io import wavfile
 
-from infer_pack.models import (
+now_dir = os.getcwd()
+
+from src.infer_pack.models import (
     SynthesizerTrnMs256NSFsid,
     SynthesizerTrnMs256NSFsid_nono,
     SynthesizerTrnMs768NSFsid,
     SynthesizerTrnMs768NSFsid_nono,
 )
-from my_utils import load_audio
-from vc_infer_pipeline import VC
-
-now_dir = os.getcwd()
+from src.my_utils import load_audio
+from src.vc_infer_pipeline import VC
 
 
 class Config:
