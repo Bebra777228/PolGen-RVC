d7742dd6ad88a22cdab618df342ea5393ce174fd
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-28 17:15:41 +0500

    added param "use_effects"

--- src/audio_effects.py
+++ src/audio_effects.py
@@ -10,6 +10,7 @@ from pedalboard.io import AudioFile
 from pydub import AudioSegment
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+OUTPUT_DIR = os.path.join(BASE_DIR, 'song_output')
 
 def display_progress(percent, message, progress=gr.Progress()):
     progress(percent, desc=message)
@@ -38,7 +39,7 @@ def convert_to_stereo(input_path, output_path):
 def add_audio_effects(vocal_audio_path, instrumental_audio_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping, reverb_width,
                       low_shelf_gain, high_shelf_gain, compressor_ratio, compressor_threshold, noise_gate_threshold, noise_gate_ratio,
                       noise_gate_attack, noise_gate_release, chorus_rate_hz, chorus_depth, chorus_centre_delay_ms, chorus_feedback,
-                      chorus_mix, output_format, vocal_gain, instrumental_gain, progress=gr.Progress()):
+                      chorus_mix, output_format, vocal_gain, instrumental_gain, use_effects, progress=gr.Progress()):
 
     if not vocal_audio_path or not instrumental_audio_path:
         raise ValueError("Оба пути к аудиофайлам должны быть заполнены.")
@@ -47,31 +48,32 @@ def add_audio_effects(vocal_audio_path, instrumental_audio_path, reverb_rm_size,
     stereo_vocal_path = 'Vocal_Stereo.wav'
     convert_to_stereo(vocal_audio_path, stereo_vocal_path)
 
-    display_progress(0.2, "Применение аудиоэффектов к вокалу...", progress)
-    board = Pedalboard(
-        [
-            HighpassFilter(),
-            Compressor(ratio=compressor_ratio, threshold_db=compressor_threshold),
-            NoiseGate(threshold_db=noise_gate_threshold, ratio=noise_gate_ratio, attack_ms=noise_gate_attack, release_ms=noise_gate_release),
-            Reverb(room_size=reverb_rm_size, dry_level=reverb_dry, wet_level=reverb_wet, damping=reverb_damping, width=reverb_width),
-            LowShelfFilter(gain_db=low_shelf_gain),
-            HighShelfFilter(gain_db=high_shelf_gain),
-            Chorus(rate_hz=chorus_rate_hz, depth=chorus_depth, centre_delay_ms=chorus_centre_delay_ms, feedback=chorus_feedback, mix=chorus_mix),
-         ]
-    )
+    if use_effects:
+        display_progress(0.2, "Применение аудиоэффектов к вокалу...", progress)
+        board = Pedalboard(
+            [
+                HighpassFilter(),
+                Compressor(ratio=compressor_ratio, threshold_db=compressor_threshold),
+                NoiseGate(threshold_db=noise_gate_threshold, ratio=noise_gate_ratio, attack_ms=noise_gate_attack, release_ms=noise_gate_release),
+                Reverb(room_size=reverb_rm_size, dry_level=reverb_dry, wet_level=reverb_wet, damping=reverb_damping, width=reverb_width),
+                LowShelfFilter(gain_db=low_shelf_gain),
+                HighShelfFilter(gain_db=high_shelf_gain),
+                Chorus(rate_hz=chorus_rate_hz, depth=chorus_depth, centre_delay_ms=chorus_centre_delay_ms, feedback=chorus_feedback, mix=chorus_mix),
+            ]
+        )
 
-    vocal_output_path = os.path.join(BASE_DIR, 'Vocal_Effects.wav')
-    with AudioFile(stereo_vocal_path) as f:
-        with AudioFile(vocal_output_path, 'w', f.samplerate, 2) as o:
-            while f.tell() < f.frames:
-                chunk = f.read(int(f.samplerate))
-                effected = board(chunk, f.samplerate, reset=False)
-                o.write(effected)
+        vocal_output_path = os.path.join(OUTPUT_DIR, 'Vocal.wav')
+        with AudioFile(stereo_vocal_path) as f:
+            with AudioFile(vocal_output_path, 'w', f.samplerate, 2) as o:
+                while f.tell() < f.frames:
+                    chunk = f.read(int(f.samplerate))
+                    effected = board(chunk, f.samplerate, reset=False)
+                    o.write(effected)
+    else:
+        vocal_output_path = stereo_vocal_path
 
     display_progress(0.5, "Объединение вокала и инструментальной части...", progress)
-    output_dir = os.path.join(BASE_DIR, 'processed_output')
-    os.makedirs(output_dir, exist_ok=True)
-    combined_output_path = os.path.join(output_dir, f'AiCover_combined.{output_format}')
+    combined_output_path = os.path.join(OUTPUT_DIR, f'AiCover_combined.{output_format}')
 
     if os.path.exists(combined_output_path):
         os.remove(combined_output_path)
