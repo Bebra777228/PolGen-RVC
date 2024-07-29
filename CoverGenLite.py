990269a9e3f67a7350fb59735ba076d587bbc320
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-30 00:38:10 +0500

    Update CoverGenLite.py

--- CoverGenLite.py
+++ CoverGenLite.py
@@ -1,15 +1,12 @@
 import os
 import gradio as gr
 
-from src.modules.model_management import *
-from src.tabs import *
-
-now_dir = os.getcwd()
-rvc_models_dir = os.path.join(now_dir, 'models', 'rvc_models')
+from src.tabs.welcome import *
+from src.tabs.conversion import *
+from src.tabs.processing import *
+from src.tabs.model_install import *
 
 if __name__ == '__main__':
-    voice_models = get_folders(rvc_models_dir)
-
     with gr.Blocks(title='CoverGen Lite - Politrees (v1.0)', theme=gr.themes.Soft(primary_hue="green", secondary_hue="green", neutral_hue="neutral", spacing_size="sm", radius_size="lg")) as app:
         
         with gr.Tab("Велком/Контакты"):
