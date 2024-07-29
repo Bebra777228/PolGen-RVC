6c4266a7a1410e560eb1adb9950e4c793c9abd8a
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-29 16:47:27 +0500

    Update voice_conversion.py

--- src/scripts/voice_conversion.py
+++ src/scripts/voice_conversion.py
@@ -34,7 +34,7 @@ def convert_to_stereo(audio_path):
 def display_progress(percent, message, progress=gr.Progress()):
     progress(percent, desc=message)
 
-def voice_change(voice_model, vocals_path, output_path, pitch_change, f0_method, index_rate, filter_radius, rms_mix_rate, protect, crepe_hop_length, f0_min, f0_max):
+def voice_change(voice_model, vocals_path, output_path, pitch_change, f0_method, index_rate, filter_radius, rms_mix_rate, protect, crepe_hop_length, f0autotune, f0_min, f0_max):
     rvc_model_path, rvc_index_path = get_rvc_model(voice_model)
     device = 'cuda:0'
     config = Config(device, True)
@@ -42,14 +42,14 @@ def voice_change(voice_model, vocals_path, output_path, pitch_change, f0_method,
     cpt, version, net_g, tgt_sr, vc = get_vc(device, config.is_half, config, rvc_model_path)
 
     rvc_infer(rvc_index_path, index_rate, vocals_path, output_path, pitch_change, f0_method, cpt, version, net_g,
-              filter_radius, tgt_sr, rms_mix_rate, protect, crepe_hop_length, vc, hubert_model, f0_min, f0_max)
+              filter_radius, tgt_sr, rms_mix_rate, protect, crepe_hop_length, vc, hubert_model, f0autotune, f0_min, f0_max)
 
     del hubert_model, cpt, net_g, vc
     gc.collect()
     torch.cuda.empty_cache()
 
 def conversion(uploaded_file, voice_model, pitch_change, index_rate=0.5, filter_radius=3, rms_mix_rate=0.25, f0_method='rmvpe',
-                        crepe_hop_length=128, protect=0.33, output_format='mp3', progress=gr.Progress(), f0_min=50, f0_max=1100):
+                        crepe_hop_length=128, protect=0.33, output_format='mp3', progress=gr.Progress(), f0autotune=False, f0_min=50, f0_max=1100):
     if not uploaded_file or not voice_model:
         raise ValueError('Убедитесь, что поле ввода песни и поле модели голоса заполнены.')
 
@@ -66,7 +66,7 @@ def conversion(uploaded_file, voice_model, pitch_change, index_rate=0.5, filter_
 
     display_progress(0.5, '[~] Преобразование вокала...', progress)
     voice_change(voice_model, orig_song_path, ai_cover_path, pitch_change, f0_method, index_rate,
-                 filter_radius, rms_mix_rate, protect, crepe_hop_length, f0_min, f0_max)
+                 filter_radius, rms_mix_rate, protect, crepe_hop_length, f0autotune, f0_min, f0_max)
 
     display_progress(1.0, '[✓] AI-кавер успешно сгенерирован!', progress)
     return ai_cover_path
