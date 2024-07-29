1572dd52159c0f22a2352aba40d83c2d3d369fba
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-29 16:42:50 +0500

    Update vc_infer_pipeline.py

--- src/vc_infer_pipeline.py
+++ src/vc_infer_pipeline.py
@@ -77,6 +77,39 @@ class VC(object):
         self.t_max = self.sr * self.x_max
         self.device = config.device
         
+        self.ref_freqs = [
+            65.41,
+            82.41,
+            110.00,
+            146.83,
+            196.00,
+            246.94,
+            329.63,
+            440.00,
+            587.33,
+            783.99,
+            1046.50,
+        ]
+        self.note_dict = self.generate_interpolated_frequencies()
+
+    def generate_interpolated_frequencies(self):
+        note_dict = []
+        for i in range(len(self.ref_freqs) - 1):
+            freq_low = self.ref_freqs[i]
+            freq_high = self.ref_freqs[i + 1]
+            interpolated_freqs = np.linspace(
+                freq_low, freq_high, num=10, endpoint=False
+            )
+            note_dict.extend(interpolated_freqs)
+        note_dict.append(self.ref_freqs[-1])
+        return note_dict
+
+    def autotune_f0(self, f0):
+        autotuned_f0 = np.zeros_like(f0)
+        for i, freq in enumerate(f0):
+            closest_note = min(self.note_dict, key=lambda x: abs(x - freq))
+            autotuned_f0[i] = closest_note
+        return autotuned_f0
 
     def get_optimal_torch_device(self, index: int = 0) -> torch.device:
         if torch.cuda.is_available():
@@ -220,6 +253,7 @@ class VC(object):
         f0_method,
         filter_radius,
         crepe_hop_length,
+        f0autotune,
         inp_f0=None,
         f0_min=50,
         f0_max=1100,
@@ -310,6 +344,10 @@ class VC(object):
                 crepe_hop_length,
                 time_step,
             )
+
+        print("f0_autotune =", f0autotune)
+        if f0autotune == "True":
+            f0 = self.autotune_f0(f0)
 
         f0 *= pow(2, f0_up_key / 12)
         tf0 = self.sr // self.window
@@ -468,6 +506,7 @@ class VC(object):
         version,
         protect,
         crepe_hop_length,
+        f0autotune,
         f0_file=None,
         f0_min=50,
         f0_max=1100,
@@ -525,6 +564,7 @@ class VC(object):
                 f0_method,
                 filter_radius,
                 crepe_hop_length,
+                f0autotune,
                 inp_f0,
                 f0_min,
                 f0_max,
