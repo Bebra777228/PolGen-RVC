362df99021a48c179af246d29f058b49bd6c56d9
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-29 12:21:14 +0500

    Update CoverGenLite.py

--- CoverGenLite.py
+++ CoverGenLite.py
@@ -15,7 +15,7 @@ rvc_models_dir = os.path.join(now_dir, 'rvc_models')
 
 
 if __name__ == '__main__':
-    voice_models = ignore_files(rvc_models_dir)
+    voice_models = get_folders(rvc_models_dir)
 
     with gr.Blocks(title='CoverGen Lite - Politrees (v0.4)', theme=gr.themes.Soft(primary_hue="green", secondary_hue="green", neutral_hue="neutral", spacing_size="sm", radius_size="lg")) as app:
         with gr.Tab("Велком/Контакты"):
