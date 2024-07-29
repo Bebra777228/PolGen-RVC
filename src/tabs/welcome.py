051ada18338ff8e5459a4ec5e206cf31b9b27a59
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-29 23:31:09 +0500

    Update welcome.py

--- src/tabs/welcome.py
+++ src/tabs/welcome.py
@@ -1,15 +1,14 @@
 import gradio as gr
 
 def welcome_tab():
-  with gr.Tab("Велком/Контакты"):
-      gr.HTML("<center><h1>Добро пожаловать в CoverGen Lite - Politrees (v1.0)</h1></center>")
-      with gr.Row():
-          with gr.Column(variant='panel'):
-              gr.HTML("<center><h2><a href='https://t.me/Politrees2'>Telegram ЛС</a></h2></center>")
-              gr.HTML("<center><h2><a href='https://vk.com/artem__bebroy'>ВКонтакте (страница)</a></h2></center>")
-          with gr.Column(variant='panel'):
-              gr.HTML("<center><h2><a href='https://t.me/pol1trees'>Telegram Канал</a></h2></center>")
-              gr.HTML("<center><h2><a href='https://t.me/+GMTP7hZqY0E4OGRi'>Telegram Чат</a></h2></center>")
-      with gr.Column(variant='panel'):
-          gr.HTML("<center><h2><a href='https://www.youtube.com/channel/UCHb3fZEVxUisnqLqCrEM8ZA'>YouTube</a></h2></center>")
-          gr.HTML("<center><h2><a href='https://github.com/Bebra777228/Pol-Litres-RVC'>GitHub</a></h2></center>")
+    gr.HTML("<center><h1>Добро пожаловать в CoverGen Lite - Politrees (v1.0)</h1></center>")
+    with gr.Row():
+        with gr.Column(variant='panel'):
+            gr.HTML("<center><h2><a href='https://t.me/Politrees2'>Telegram ЛС</a></h2></center>")
+            gr.HTML("<center><h2><a href='https://vk.com/artem__bebroy'>ВКонтакте (страница)</a></h2></center>")
+        with gr.Column(variant='panel'):
+            gr.HTML("<center><h2><a href='https://t.me/pol1trees'>Telegram Канал</a></h2></center>")
+            gr.HTML("<center><h2><a href='https://t.me/+GMTP7hZqY0E4OGRi'>Telegram Чат</a></h2></center>")
+    with gr.Column(variant='panel'):
+        gr.HTML("<center><h2><a href='https://www.youtube.com/channel/UCHb3fZEVxUisnqLqCrEM8ZA'>YouTube</a></h2></center>")
+        gr.HTML("<center><h2><a href='https://github.com/Bebra777228/Pol-Litres-RVC'>GitHub</a></h2></center>")
