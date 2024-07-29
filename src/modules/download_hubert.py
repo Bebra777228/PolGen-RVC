ebef2ca049a25c4f000ef4943b1d263f269ee6aa
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-29 16:18:23 +0500

    Update download_hubert.py

--- src/modules/download_hubert.py
+++ src/modules/download_hubert.py
@@ -10,13 +10,13 @@ hubert_base_path = os.path.join(rvc_models_dir, 'hubert_base.pt')
 base_url = 'https://huggingface.co/Politrees/all_RVC-pretrained_and_other/resolve/main/HuBERTs/'
 
 models = {
-    'Стандарт - hubert_base': 'hubert_base.pt',
-    'Улучшенный стандарт - contentvec_base': 'contentvec_base.pt',
-    'Корейский базовый - korean_hubert_base': 'korean_hubert_base.pt',
-    'Китайский базовый - chinese_hubert_base': 'chinese_hubert_base.pt',
-    'Китайский большой - chinese_hubert_large': 'chinese_hubert_large.pt',
-    'Японский базовый - japanese_hubert_base': 'japanese_hubert_base.pt',
-    'Японский большой - japanese_hubert_large': 'japanese_hubert_large.pt'
+    'Стандартный hubert': 'hubert_base.pt',
+    'СontentVec': 'contentvec_base.pt',
+    'Корейский hubert_base': 'korean_hubert_base.pt',
+    'Китайский hubert_base': 'chinese_hubert_base.pt',
+    'Китайский hubert_large': 'chinese_hubert_large.pt',
+    'Японский hubert_base': 'japanese_hubert_base.pt',
+    'Японский hubert_large': 'japanese_hubert_large.pt'
 }
 
 def download_and_replace_model(model_desc, progress=gr.Progress()):
