85a3c0902d2ab15305f914e10d9f27febe61ac16
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-31 00:10:05 +0500

    Update audio_processing.py

--- src/scripts/audio_processing.py
+++ src/scripts/audio_processing.py
@@ -6,6 +6,9 @@ import soundfile as sf
 from pedalboard import Pedalboard, Reverb, Compressor, HighpassFilter, LowShelfFilter, HighShelfFilter, NoiseGate, Chorus
 from pedalboard.io import AudioFile
 from pydub import AudioSegment
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 now_dir = os.getcwd()
 RVC_MODELS_DIR = os.path.join(now_dir, 'models', 'rvc_models')
@@ -15,32 +18,32 @@ def display_progress(percent, message, progress=gr.Progress()):
     progress(percent, desc=message)
 
 def combine_audio(vocal_path, instrumental_path, output_path, vocal_gain, instrumental_gain, output_format):
-    vocal = AudioSegment.from_file(vocal_path) + vocal_gain
-    instrumental = AudioSegment.from_file(instrumental_path) + instrumental_gain
-    combined = vocal.overlay(instrumental)
-    combined.export(output_path, format=output_format)
+    try:
+        vocal = AudioSegment.from_file(vocal_path) + vocal_gain
+        instrumental = AudioSegment.from_file(instrumental_path) + instrumental_gain
+        combined = vocal.overlay(instrumental)
+        combined.export(output_path, format=output_format)
+    except Exception as e:
+        logging.error(f"Ошибка при объединении аудио: {e}")
+        raise
 
 def convert_to_stereo(input_path, output_path):
-    y, sr = librosa.load(input_path, sr=None, mono=False)
-    if y.ndim == 1:
-        y = np.vstack([y, y])
-    elif y.ndim > 2:
-        y = y[:2, :]
-    sf.write(output_path, y.T, sr, format='WAV')
+    try:
+        y, sr = librosa.load(input_path, sr=None, mono=False)
+        if y.ndim == 1:
+            y = np.vstack([y, y])
+        elif y.ndim > 2:
+            y = y[:2, :]
+        sf.write(output_path, y.T, sr, format='WAV')
+    except Exception as e:
+        logging.error(f"Ошибка при конвертации в стерео: {e}")
+        raise
 
-def processing(vocal_audio_path, instrumental_audio_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping, reverb_width,
-               low_shelf_gain, high_shelf_gain, compressor_ratio, compressor_threshold, noise_gate_threshold, noise_gate_ratio,
-               noise_gate_attack, noise_gate_release, chorus_rate_hz, chorus_depth, chorus_centre_delay_ms, chorus_feedback,
-               chorus_mix, output_format, vocal_gain, instrumental_gain, use_effects, progress=gr.Progress()):
-
-    if not vocal_audio_path or not instrumental_audio_path:
-        raise ValueError("Оба пути к аудиофайлам должны быть заполнены.")
-
-    stereo_vocal_path = os.path.join(OUTPUT_DIR, 'Voice_stereo.wav')
-    convert_to_stereo(vocal_audio_path, stereo_vocal_path)
-
-    if use_effects:
-        display_progress(0.2, "Применение аудиоэффектов к вокалу...", progress)
+def apply_effects(vocal_path, output_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping, reverb_width,
+                  low_shelf_gain, high_shelf_gain, compressor_ratio, compressor_threshold, noise_gate_threshold,
+                  noise_gate_ratio, noise_gate_attack, noise_gate_release, chorus_rate_hz, chorus_depth,
+                  chorus_centre_delay_ms, chorus_feedback, chorus_mix):
+    try:
         board = Pedalboard([
             HighpassFilter(),
             Compressor(ratio=compressor_ratio, threshold_db=compressor_threshold),
@@ -50,24 +53,48 @@ def processing(vocal_audio_path, instrumental_audio_path, reverb_rm_size, reverb
             HighShelfFilter(gain_db=high_shelf_gain),
             Chorus(rate_hz=chorus_rate_hz, depth=chorus_depth, centre_delay_ms=chorus_centre_delay_ms, feedback=chorus_feedback, mix=chorus_mix),
         ])
-
-        vocal_output_path = os.path.join(OUTPUT_DIR, 'Vocal.wav')
-        with AudioFile(stereo_vocal_path) as f, AudioFile(vocal_output_path, 'w', f.samplerate, 2) as o:
+        
+        with AudioFile(vocal_path) as f, AudioFile(output_path, 'w', f.samplerate, 2) as o:
             while f.tell() < f.frames:
                 chunk = f.read(int(f.samplerate))
                 effected = board(chunk, f.samplerate, reset=False)
                 o.write(effected)
-    else:
-        vocal_output_path = stereo_vocal_path
+    except Exception as e:
+        logging.error(f"Ошибка при применении эффектов: {e}")
+        raise
 
-    display_progress(0.5, "Объединение вокала и инструментальной части...", progress)
-    combined_output_path = os.path.join(OUTPUT_DIR, f'AiCover.{output_format}')
+def processing(vocal_audio_path, instrumental_audio_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping, reverb_width,
+               low_shelf_gain, high_shelf_gain, compressor_ratio, compressor_threshold, noise_gate_threshold, noise_gate_ratio,
+               noise_gate_attack, noise_gate_release, chorus_rate_hz, chorus_depth, chorus_centre_delay_ms, chorus_feedback,
+               chorus_mix, output_format, vocal_gain, instrumental_gain, use_effects, progress=gr.Progress()):
 
-    if os.path.exists(combined_output_path):
-        os.remove(combined_output_path)
+    try:
+        if not vocal_audio_path or not instrumental_audio_path:
+            raise ValueError("Оба пути к аудиофайлам должны быть заполнены.")
 
-    combine_audio(vocal_output_path, instrumental_audio_path, combined_output_path, vocal_gain, instrumental_gain, output_format)
+        stereo_vocal_path = os.path.join(OUTPUT_DIR, 'Voice_stereo.wav')
+        convert_to_stereo(vocal_audio_path, stereo_vocal_path)
 
-    display_progress(1.0, "Готово!", progress)
+        if use_effects:
+            display_progress(0.2, "Применение аудиоэффектов к вокалу...", progress)
+            vocal_output_path = os.path.join(OUTPUT_DIR, 'Vocal.wav')
+            apply_effects(stereo_vocal_path, vocal_output_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping, reverb_width,
+                          low_shelf_gain, high_shelf_gain, compressor_ratio, compressor_threshold, noise_gate_threshold, noise_gate_ratio,
+                          noise_gate_attack, noise_gate_release, chorus_rate_hz, chorus_depth, chorus_centre_delay_ms, chorus_feedback, chorus_mix)
+        else:
+            vocal_output_path = stereo_vocal_path
 
-    return combined_output_path
+        display_progress(0.5, "Объединение вокала и инструментальной части...", progress)
+        combined_output_path = os.path.join(OUTPUT_DIR, f'AiCover.{output_format}')
+
+        if os.path.exists(combined_output_path):
+            os.remove(combined_output_path)
+
+        combine_audio(vocal_output_path, instrumental_audio_path, combined_output_path, vocal_gain, instrumental_gain, output_format)
+
+        display_progress(1.0, "Готово!", progress)
+
+        return combined_output_path
+    except Exception as e:
+        logging.error(f"Ошибка в процессе обработки: {e}")
+        raise
