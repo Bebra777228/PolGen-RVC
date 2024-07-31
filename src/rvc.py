5a6564c2a24c1ad75d44beba7b00af77a71fcd77
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-31 22:00:39 +0500

    Update rvc.py

--- src/rvc.py
+++ src/rvc.py
@@ -161,7 +161,6 @@ def rvc_infer(
     hop_length,
     vc,
     hubert_model,
-    f0autotune,
     f0_min=50,
     f0_max=1100
 ):
@@ -185,7 +184,6 @@ def rvc_infer(
         version,
         protect,
         hop_length,
-        f0autotune,
         f0_file=None,
         f0_min=f0_min,
         f0_max=f0_max
