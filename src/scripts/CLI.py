440ed4a0e1e2c00fe16db004e8911ec98d727c46
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-31 19:58:19 +0500

    Update CLI.py

--- src/scripts/CLI.py
+++ src/scripts/CLI.py
@@ -21,7 +21,7 @@ parser.add_argument('-ir', '--index_rate', type=float, default=0)
 parser.add_argument('-fr', '--filter_radius', type=int, default=3)
 parser.add_argument('-rms', '--rms_mix_rate', type=float, default=0.25)
 parser.add_argument('-m', '--method', type=str, default='rmvpe')
-parser.add_argument('-hop', '--crepe_hop_length', type=int, default=128)
+parser.add_argument('-hop', '--hop_length', type=int, default=128)
 parser.add_argument('-pro', '--protect', type=float, default=0.33)
 parser.add_argument('-f', '--format', type=str, default='mp3')
 args = parser.parse_args()
@@ -34,7 +34,7 @@ cover_path = conversion(
     args.song_input, rvc_dirname, args.pitch,
     index_rate=args.index_rate, filter_radius=args.filter_radius,
     rms_mix_rate=args.rms_mix_rate, f0_method=args.method,
-    crepe_hop_length=args.crepe_hop_length, protect=args.protect,
+    hop_length=args.hop_length, protect=args.protect,
     output_format=args.format
 )
 
