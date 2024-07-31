227dc8c673a3414431bcd05b8c76475d8bef8a90
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-07-31 21:11:22 +0500

    Update ui_updates.py

--- src/modules/ui_updates.py
+++ src/modules/ui_updates.py
@@ -1,22 +1,15 @@
 import gradio as gr
-    
+
 
 def process_file_upload(file):
     return file.name, gr.update(value=file.name)
-    
+
 
 def show_hop_slider(pitch_detection_algo):
     if pitch_detection_algo in ['mangio-crepe']:
         return gr.update(visible=True)
     else:
         return gr.update(visible=False)
-
-
-def update_f0_method(use_hybrid_methods):
-    if use_hybrid_methods:
-        return gr.update(choices=['hybrid[rmvpe+fcpe]', 'hybrid[rmvpe+crepe]', 'hybrid[crepe+rmvpe]', 'hybrid[crepe+fcpe]', 'hybrid[crepe+rmvpe+fcpe]'], value='hybrid[rmvpe+fcpe]')
-    else:
-        return gr.update(choices=['rmvpe+', 'fcpe', 'rmvpe', 'mangio-crepe', 'crepe'], value='rmvpe+')
 
 
 def update_button_text():
