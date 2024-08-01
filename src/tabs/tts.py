fd142c8778ff408481171866af47fa3938b3e896
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-08-02 01:28:01 +0500

    Update tts.py

--- src/tabs/tts.py
+++ src/tabs/tts.py
@@ -7,52 +7,11 @@ from src.scripts.tts_conversion import tts_conversion
 from src.modules.model_management import *
 from src.modules.ui_updates import *
 from src.modules.download_hubert import *
+from src.modules.tts_voice import *
 
 rvc_models_dir = os.path.join(now_dir, 'models', 'rvc_models')
 output_dir = os.path.join(now_dir, 'song_output')
 voice_models = get_folders(rvc_models_dir)
-
-
-voices = {
-    "Английский (Великобритания)": ["en-GB-SoniaNeural", "en-GB-RyanNeural"],
-    "Английский (США)": ["en-US-JennyNeural", "en-US-GuyNeural"],
-    "Арабский (Египет)": ["ar-EG-SalmaNeural", "ar-EG-ShakirNeural"],
-    "Арабский (Саудовская Аравия)": ["ar-SA-HamedNeural", "ar-SA-ZariyahNeural"],
-    "Бенгальский (Бангладеш)": ["bn-BD-RubaiyatNeural", "bn-BD-KajalNeural"],
-    "Венгерский": ["hu-HU-TamasNeural", "hu-HU-NoemiNeural"],
-    "Вьетнамский": ["vi-VN-HoaiMyNeural", "vi-VN-HuongNeural"],
-    "Греческий": ["el-GR-AthinaNeural", "el-GR-NestorasNeural"],
-    "Датский": ["da-DK-PernilleNeural", "da-DK-MadsNeural"],
-    "Иврит": ["he-IL-AvriNeural", "he-IL-HilaNeural"],
-    "Испанский (Испания)": ["es-ES-ElviraNeural", "es-ES-AlvaroNeural"],
-    "Испанский (Мексика)": ["es-MX-DaliaNeural", "es-MX-JorgeNeural"],
-    "Итальянский": ["it-IT-ElsaNeural", "it-IT-DiegoNeural"],
-    "Китайский (упрощенный)": ["zh-CN-XiaoxiaoNeural", "zh-CN-YunxiNeural"],
-    "Корейский": ["ko-KR-SunHiNeural", "ko-KR-InJoonNeural"],
-    "Немецкий": ["de-DE-KatjaNeural", "de-DE-ConradNeural"],
-    "Нидерландский": ["nl-NL-ColetteNeural", "nl-NL-FennaNeural"],
-    "Норвежский": ["nb-NO-PernilleNeural", "nb-NO-FinnNeural"],
-    "Польский": ["pl-PL-MajaNeural", "pl-PL-JacekNeural"],
-    "Португальский (Бразилия)": ["pt-BR-FranciscaNeural", "pt-BR-AntonioNeural"],
-    "Португальский (Португалия)": ["pt-PT-RaquelNeural", "pt-PT-DuarteNeural"],
-    "Румынский": ["ro-RO-EmilNeural", "ro-RO-AndreiNeural"],
-    "Русский": ["ru-RU-SvetlanaNeural", "ru-RU-DmitryNeural"],
-    "Тагальский": ["tl-PH-AngeloNeural", "tl-PH-TessaNeural"],
-    "Тамильский": ["ta-IN-ValluvarNeural", "ta-IN-KannanNeural"],
-    "Тайский": ["th-TH-PremwadeeNeural", "th-TH-NiwatNeural"],
-    "Турецкий": ["tr-TR-AhmetNeural", "tr-TR-EmelNeural"],
-    "Украинский": ["uk-UA-OstapNeural", "uk-UA-PolinaNeural"],
-    "Филиппинский": ["fil-PH-AngeloNeural", "fil-PH-TessaNeural"],
-    "Финский": ["fi-FI-NooraNeural", "fi-FI-SelmaNeural"],
-    "Французский (Канада)": ["fr-CA-SylvieNeural", "fr-CA-AntoineNeural"],
-    "Французский (Франция)": ["fr-FR-DeniseNeural", "fr-FR-HenriNeural"],
-    "Чешский": ["cs-CZ-VlastaNeural", "cs-CZ-AntoninNeural"],
-    "Шведский": ["sv-SE-HilleviNeural", "sv-SE-MattiasNeural"],
-    "Японский": ["ja-JP-NanamiNeural", "ja-JP-KeitaNeural"],
-}
-
-def update_voices(selected_language):
-    return gr.update(choices=voices[selected_language])
 
 
 def edge_tts_tab():
