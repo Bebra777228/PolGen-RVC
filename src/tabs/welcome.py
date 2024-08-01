5e1efe2110868d6eb790a1bedd665d59ec7adc04
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-08-02 01:59:57 +0500

    Update welcome.py

--- src/tabs/welcome.py
+++ src/tabs/welcome.py
@@ -13,4 +13,4 @@ def welcome_tab():
         gr.HTML("<center><h2><a href='https://www.youtube.com/channel/UCHb3fZEVxUisnqLqCrEM8ZA'>YouTube</a></h2></center>")
         gr.HTML("<center><h2><a href='https://github.com/Bebra777228/Pol-Litres-RVC'>GitHub</a></h2></center>")
     with gr.Column(variant='panel'):
-        gr.HTML("<center><h2>Спасбибо <a href='https://t.me/Player1444'>Player1444</a> за помощь в тестах.</h2></center>")
+        gr.HTML("<center><h2>Спасибо <a href='https://t.me/Player1444'>Player1444</a> за помощь в тестах.</h2></center>")
