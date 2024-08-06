68ca72e3b3ba616760a16b9ef4b5a79b5086de8f
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-08-06 12:19:29 +0500

    Update tts_conversion.py

--- src/scripts/tts_conversion.py
+++ src/scripts/tts_conversion.py
@@ -17,6 +17,9 @@ RVC_MODELS_DIR = os.path.join(now_dir, 'models', 'rvc_models')
 HUBERT_MODEL_PATH = os.path.join(now_dir, 'models', 'assets', 'hubert_base.pt')
 OUTPUT_DIR = os.path.join(now_dir, 'output')
 
+def display_progress(percent, message, progress=gr.Progress()):
+    progress(percent, desc=message)
+
 def get_rvc_model(voice_model):
     model_dir = os.path.join(RVC_MODELS_DIR, voice_model)
     rvc_model_path = next((os.path.join(model_dir, f) for f in os.listdir(model_dir) if f.endswith('.pth')), None)
@@ -25,16 +28,9 @@ def get_rvc_model(voice_model):
         raise ValueError(f'В каталоге {model_dir} отсутствует файл модели.')
     return rvc_model_path, rvc_index_path
 
-def convert_to_stereo(audio_path):
-    wave, sr = librosa.load(audio_path, mono=False, sr=44100)
-    if wave.ndim == 1:
-        stereo_path = os.path.join(OUTPUT_DIR, 'Voice_stereo.wav')
-        subprocess.run(shlex.split(f'ffmpeg -y -loglevel error -i "{audio_path}" -ac 2 -f wav "{stereo_path}"'))
-        return stereo_path
-    return audio_path
-
-def display_progress(percent, message, progress=gr.Progress()):
-    progress(percent, desc=message)
+async def text_to_speech(text, voice, output_path):
+    communicate = edge_tts.Communicate(text=text, voice=voice)
+    await communicate.save(output_path)
 
 def voice_change(voice_model, vocals_path, output_path, pitch_change, f0_method, index_rate, filter_radius, volume_envelope, protect, hop_length, f0autotune, f0_min, f0_max):
     rvc_model_path, rvc_index_path = get_rvc_model(voice_model)
@@ -50,9 +46,6 @@ def voice_change(voice_model, vocals_path, output_path, pitch_change, f0_method,
     gc.collect()
     torch.cuda.empty_cache()
 
-async def text_to_speech(text, voice, output_path):
-    communicate = edge_tts.Communicate(text=text, voice=voice)
-    await communicate.save(output_path)
 
 def tts_conversion(text, voice_model, voice, pitch_change, index_rate=0.5, filter_radius=3, volume_envelope=0.25, f0_method='rmvpe',
                    hop_length=128, protect=0.33, output_format='mp3', progress=gr.Progress(), f0autotune=False, f0_min=50, f0_max=1100):
@@ -61,7 +54,7 @@ def tts_conversion(text, voice_model, voice, pitch_change, index_rate=0.5, filte
 
     display_progress(0, '[~] Запуск конвейера генерации TTS...', progress)
 
-    tts_voice_path = os.path.join(OUTPUT_DIR, f'TTS_Output.wav')
+    tts_voice_path = os.path.join(OUTPUT_DIR, f'TTS_Voice.wav')
     asyncio.run(text_to_speech(text, voice, tts_voice_path))
 
     display_progress(0.5, '[~] Преобразование голоса...', progress)
