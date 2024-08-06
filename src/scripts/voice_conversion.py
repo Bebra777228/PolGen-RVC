0e03fcbf31855fcddbc5a3c74cddf0bc7941d232
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-08-06 16:39:27 +0500

    Update voice_conversion.py

--- src/scripts/voice_conversion.py
+++ src/scripts/voice_conversion.py
@@ -37,22 +37,29 @@ def convert_audio_to_stereo(audio_path):
         return stereo_path
     return audio_path
 
-def perform_voice_conversion(voice_model, vocals_path, output_path, pitch, f0_method, index_rate, filter_radius, volume_envelope, protect, hop_length, f0_autotune, f0_min, f0_max):
+def perform_voice_conversion(
+    voice_model, vocals_path, output_path, pitch, f0_method, index_rate, filter_radius, volume_envelope, protect, hop_length, f0_autotune, f0_min, f0_max
+):
     rvc_model_path, rvc_index_path = load_rvc_model(voice_model)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     config = Config(device, True)
     hubert_model = load_hubert(device, config.is_half, HUBERT_MODEL_PATH)
     cpt, version, net_g, tgt_sr, vc = get_vc(device, config.is_half, config, rvc_model_path)
 
-    rvc_infer(rvc_index_path, index_rate, vocals_path, output_path, pitch, f0_method, cpt, version, net_g,
-              filter_radius, tgt_sr, volume_envelope, protect, hop_length, vc, hubert_model, f0_autotune, f0_min, f0_max)
+    rvc_infer(
+        rvc_index_path, index_rate, vocals_path, output_path, pitch, f0_method, cpt, version, net_g,
+        filter_radius, tgt_sr, volume_envelope, protect, hop_length, vc, hubert_model, f0_autotune, f0_min, f0_max
+    )
 
     del hubert_model, cpt, net_g, vc
     gc.collect()
     torch.cuda.empty_cache()
 
-def voice_pipeline(uploaded_file, voice_model, pitch, index_rate=0.5, filter_radius=3, volume_envelope=0.25, f0_method='rmvpe',
-                   hop_length=128, protect=0.33, output_format='mp3', progress=gr.Progress(), f0_autotune=False, f0_min=50, f0_max=1100):
+def voice_pipeline(
+    uploaded_file, voice_model, pitch, index_rate=0.5, filter_radius=3, volume_envelope=0.25, f0_method='rmvpe',
+    hop_length=128, protect=0.33, output_format='mp3', f0_autotune=False, f0_min=50, f0_max=1100,
+    progress=gr.Progress()
+):
     if not uploaded_file or not voice_model:
         raise ValueError('Заполните все необходимые поля.')
 
@@ -70,7 +77,9 @@ def voice_pipeline(uploaded_file, voice_model, pitch, index_rate=0.5, filter_rad
         os.remove(voice_convert_path)
 
     display_progress(0.5, '[~] Преобразование вокала...', progress)
-    perform_voice_conversion(voice_model, orig_song_path, voice_convert_path, pitch, f0_method, index_rate,
-                             filter_radius, volume_envelope, protect, hop_length, f0_autotune, f0_min, f0_max)
+    perform_voice_conversion(
+        voice_model, orig_song_path, voice_convert_path, pitch, f0_method, index_rate,
+        filter_radius, volume_envelope, protect, hop_length, f0_autotune, f0_min, f0_max
+    )
 
     return voice_convert_path
