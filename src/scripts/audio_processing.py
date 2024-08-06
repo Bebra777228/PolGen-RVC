54b91058de8297f574d9aec64695a4d82eca1e9b
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-08-06 12:36:13 +0500

    Update audio_processing.py

--- src/scripts/audio_processing.py
+++ src/scripts/audio_processing.py
@@ -14,13 +14,13 @@ OUTPUT_DIR = os.path.join(now_dir, 'output')
 def display_progress(percent, message, progress=gr.Progress()):
     progress(percent, desc=message)
 
-def combine_audio(vocal_path, instrumental_path, output_path, vocal_gain, instrumental_gain, output_format):
+def combine_audio_tracks(vocal_path, instrumental_path, output_path, vocal_gain, instrumental_gain, output_format):
     vocal = AudioSegment.from_file(vocal_path) + vocal_gain
     instrumental = AudioSegment.from_file(instrumental_path) + instrumental_gain
     combined = vocal.overlay(instrumental)
     combined.export(output_path, format=output_format)
 
-def convert_to_stereo(input_path, output_path):
+def convert_audio_to_stereo(input_path, output_path):
     y, sr = librosa.load(input_path, sr=None, mono=False)
     if y.ndim == 1:
         y = np.vstack([y, y])
@@ -28,10 +28,10 @@ def convert_to_stereo(input_path, output_path):
         y = y[:2, :]
     sf.write(output_path, y.T, sr, format='WAV')
 
-def apply_effects(vocal_path, output_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping, reverb_width,
-                  low_shelf_gain, high_shelf_gain, compressor_ratio, compressor_threshold, noise_gate_threshold,
-                  noise_gate_ratio, noise_gate_attack, noise_gate_release, chorus_rate_hz, chorus_depth,
-                  chorus_centre_delay_ms, chorus_feedback, chorus_mix):
+def apply_audio_effects(vocal_path, output_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping, reverb_width,
+                        low_shelf_gain, high_shelf_gain, compressor_ratio, compressor_threshold, noise_gate_threshold,
+                        noise_gate_ratio, noise_gate_attack, noise_gate_release, chorus_rate_hz, chorus_depth,
+                        chorus_centre_delay_ms, chorus_feedback, chorus_mix):
     board = Pedalboard([
         HighpassFilter(),
         Compressor(ratio=compressor_ratio, threshold_db=compressor_threshold),
@@ -48,22 +48,23 @@ def apply_effects(vocal_path, output_path, reverb_rm_size, reverb_wet, reverb_dr
             effected = board(chunk, f.samplerate, reset=False)
             o.write(effected)
 
-def processing(vocal_audio_path, instrumental_audio_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping, reverb_width,
-               low_shelf_gain, high_shelf_gain, compressor_ratio, compressor_threshold, noise_gate_threshold, noise_gate_ratio,
-               noise_gate_attack, noise_gate_release, chorus_rate_hz, chorus_depth, chorus_centre_delay_ms, chorus_feedback,
-               chorus_mix, output_format, vocal_gain, instrumental_gain, use_effects, progress=gr.Progress()):
+def process_audio(vocal_audio_path, instrumental_audio_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping, reverb_width,
+                  low_shelf_gain, high_shelf_gain, compressor_ratio, compressor_threshold, noise_gate_threshold, noise_gate_ratio,
+                  noise_gate_attack, noise_gate_release, chorus_rate_hz, chorus_depth, chorus_centre_delay_ms, chorus_feedback,
+                  chorus_mix, output_format, vocal_gain, instrumental_gain, use_effects, progress=gr.Progress()):
     if not vocal_audio_path or not instrumental_audio_path:
         raise ValueError("Оба пути к аудиофайлам должны быть заполнены.")
 
-    stereo_vocal_path = os.path.join(OUTPUT_DIR, 'Voice_stereo.wav')
-    convert_to_stereo(vocal_audio_path, stereo_vocal_path)
+    display_progress(0, "Конвертация вокала в стерео...", progress)
+    stereo_vocal_path = os.path.join(OUTPUT_DIR, 'Voice_Stereo.wav')
+    convert_audio_to_stereo(vocal_audio_path, stereo_vocal_path)
 
     if use_effects:
         display_progress(0.2, "Применение аудиоэффектов к вокалу...", progress)
-        vocal_output_path = os.path.join(OUTPUT_DIR, 'Vocal.wav')
-        apply_effects(stereo_vocal_path, vocal_output_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping, reverb_width,
-                      low_shelf_gain, high_shelf_gain, compressor_ratio, compressor_threshold, noise_gate_threshold, noise_gate_ratio,
-                      noise_gate_attack, noise_gate_release, chorus_rate_hz, chorus_depth, chorus_centre_delay_ms, chorus_feedback, chorus_mix)
+        vocal_output_path = os.path.join(OUTPUT_DIR, 'Vocal_Effected.wav')
+        apply_audio_effects(stereo_vocal_path, vocal_output_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping, reverb_width,
+                            low_shelf_gain, high_shelf_gain, compressor_ratio, compressor_threshold, noise_gate_threshold, noise_gate_ratio,
+                            noise_gate_attack, noise_gate_release, chorus_rate_hz, chorus_depth, chorus_centre_delay_ms, chorus_feedback, chorus_mix)
     else:
         vocal_output_path = stereo_vocal_path
 
@@ -73,6 +74,7 @@ def processing(vocal_audio_path, instrumental_audio_path, reverb_rm_size, reverb
     if os.path.exists(combined_output_path):
         os.remove(combined_output_path)
 
-    combine_audio(vocal_output_path, instrumental_audio_path, combined_output_path, vocal_gain, instrumental_gain, output_format)
+    combine_audio_tracks(vocal_output_path, instrumental_audio_path, combined_output_path, vocal_gain, instrumental_gain, output_format)
 
+    display_progress(1.0, "Процесс завершен.", progress)
     return combined_output_path
