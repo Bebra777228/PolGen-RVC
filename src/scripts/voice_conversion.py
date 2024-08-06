675293138e5f2ce14306721607c4e64d5a4529b3
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-08-06 12:31:49 +0500

    Update voice_conversion.py

--- src/scripts/voice_conversion.py
+++ src/scripts/voice_conversion.py
@@ -15,15 +15,21 @@ RVC_MODELS_DIR = os.path.join(now_dir, 'models', 'rvc_models')
 HUBERT_MODEL_PATH = os.path.join(now_dir, 'models', 'assets', 'hubert_base.pt')
 OUTPUT_DIR = os.path.join(now_dir, 'output')
 
-def get_rvc_model(voice_model):
+def display_progress(percent, message, progress=gr.Progress()):
+    progress(percent, desc=message)
+
+def load_rvc_model(voice_model):
     model_dir = os.path.join(RVC_MODELS_DIR, voice_model)
-    rvc_model_path = next((os.path.join(model_dir, f) for f in os.listdir(model_dir) if f.endswith('.pth')), None)
-    rvc_index_path = next((os.path.join(model_dir, f) for f in os.listdir(model_dir) if f.endswith('.index')), None)
+    model_files = os.listdir(model_dir)
+    rvc_model_path = next((os.path.join(model_dir, f) for f in model_files if f.endswith('.pth')), None)
+    rvc_index_path = next((os.path.join(model_dir, f) for f in model_files if f.endswith('.index')), None)
+    
     if not rvc_model_path:
-        raise ValueError(f'В каталоге {model_dir} отсутствует файл модели.')
+        raise ValueError(f'Файл модели не найден в каталоге {model_dir}.')
+    
     return rvc_model_path, rvc_index_path
 
-def convert_to_stereo(audio_path):
+def convert_audio_to_stereo(audio_path):
     wave, sr = librosa.load(audio_path, mono=False, sr=44100)
     if wave.ndim == 1:
         stereo_path = os.path.join(OUTPUT_DIR, 'Voice_stereo.wav')
@@ -31,41 +37,38 @@ def convert_to_stereo(audio_path):
         return stereo_path
     return audio_path
 
-def display_progress(percent, message, progress=gr.Progress()):
-    progress(percent, desc=message)
-
-def voice_change(voice_model, vocals_path, output_path, pitch_change, f0_method, index_rate, filter_radius, volume_envelope, protect, hop_length, f0autotune, f0_min, f0_max):
-    rvc_model_path, rvc_index_path = get_rvc_model(voice_model)
+def perform_voice_conversion(voice_model, vocals_path, output_path, pitch, f0_method, index_rate, filter_radius, volume_envelope, protect, hop_length, f0_autotune, f0_min, f0_max):
+    rvc_model_path, rvc_index_path = load_rvc_model(voice_model)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     config = Config(device, True)
     hubert_model = load_hubert(device, config.is_half, HUBERT_MODEL_PATH)
     cpt, version, net_g, tgt_sr, vc = get_vc(device, config.is_half, config, rvc_model_path)
 
-    rvc_infer(rvc_index_path, index_rate, vocals_path, output_path, pitch_change, f0_method, cpt, version, net_g,
-              filter_radius, tgt_sr, volume_envelope, protect, hop_length, vc, hubert_model, f0autotune, f0_min, f0_max)
+    rvc_infer(rvc_index_path, index_rate, vocals_path, output_path, pitch, f0_method, cpt, version, net_g,
+              filter_radius, tgt_sr, volume_envelope, protect, hop_length, vc, hubert_model, f0_autotune, f0_min, f0_max)
 
     del hubert_model, cpt, net_g, vc
     gc.collect()
     torch.cuda.empty_cache()
 
-def conversion(uploaded_file, voice_model, pitch_change, index_rate=0.5, filter_radius=3, volume_envelope=0.25, f0_method='rmvpe',
-               hop_length=128, protect=0.33, output_format='mp3', progress=gr.Progress(), f0autotune=False, f0_min=50, f0_max=1100):
+def voice_pipeline(uploaded_file, voice_model, pitch, index_rate=0.5, filter_radius=3, volume_envelope=0.25, f0_method='rmvpe',
+                      hop_length=128, protect=0.33, output_format='mp3', progress=gr.Progress(), f0_autotune=False, f0_min=50, f0_max=1100):
     if not uploaded_file or not voice_model:
-        raise ValueError('Убедитесь, что поле ввода песни и поле модели голоса заполнены.')
+        raise ValueError('Заполните все необходимые поля.')
 
     display_progress(0, '[~] Запуск конвейера генерации AI-кавера...', progress)
 
     if not os.path.exists(uploaded_file):
         raise ValueError(f'{uploaded_file} не существует.')
 
-    orig_song_path = convert_to_stereo(uploaded_file)
+    orig_song_path = convert_audio_to_stereo(uploaded_file)
     voice_convert_path = os.path.join(OUTPUT_DIR, f'Converted_Voice.{output_format}')
 
     if os.path.exists(voice_convert_path):
         os.remove(voice_convert_path)
 
     display_progress(0.5, '[~] Преобразование вокала...', progress)
-    voice_change(voice_model, orig_song_path, voice_convert_path, pitch_change, f0_method, index_rate,
-                 filter_radius, volume_envelope, protect, hop_length, f0autotune, f0_min, f0_max)
+    perform_voice_conversion(voice_model, orig_song_path, voice_convert_path, pitch, f0_method, index_rate,
+                             filter_radius, volume_envelope, protect, hop_length, f0_autotune, f0_min, f0_max)
 
     return voice_convert_path
