aecc865b08818ee6a4e72096c81b0833ec8a3171
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-08-07 15:47:22 +0500

    Update models.py

--- src/infer_pack/models.py
+++ src/infer_pack/models.py
@@ -1,5 +1,4 @@
 import math
-import os
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -19,14 +18,14 @@ class TextEncoder(nn.Module):
         super().__init__()
         self.out_channels = out_channels
         self.hidden_channels = hidden_channels
-        self.emb_phone = nn.Linear(input_dim, hidden_channels).to(device)
-        self.lrelu = nn.LeakyReLU(0.1, inplace=True).to(device)
-        self.emb_pitch = nn.Embedding(256, hidden_channels).to(device) if f0 else None
-        self.encoder = Encoder(hidden_channels, filter_channels, n_heads, n_layers, kernel_size, p_dropout).to(device)
-        self.proj = nn.Conv1d(hidden_channels, out_channels * 2, 1).to(device)
+        self.emb_phone = nn.Linear(input_dim, hidden_channels)
+        self.lrelu = nn.LeakyReLU(0.1, inplace=True)
+        self.emb_pitch = nn.Embedding(256, hidden_channels) if f0 else None
+        self.encoder = Encoder(hidden_channels, filter_channels, n_heads, n_layers, kernel_size, p_dropout)
+        self.proj = nn.Conv1d(hidden_channels, out_channels * 2, 1)
 
     def forward(self, phone, pitch, lengths):
-        x = self.emb_phone(phone) + self.emb_pitch(pitch) if pitch is not None else self.emb_phone(phone)
+        x = self.emb_phone(phone) + (self.emb_pitch(pitch) if pitch is not None else 0)
         x = x * math.sqrt(self.hidden_channels)
         x = self.lrelu(x)
         x = torch.transpose(x, 1, -1)
@@ -42,8 +41,8 @@ class ResidualCouplingBlock(nn.Module):
         super().__init__()
         self.flows = nn.ModuleList()
         for i in range(n_flows):
-            self.flows.append(ResidualCouplingLayer(channels, hidden_channels, kernel_size, dilation_rate, n_layers, gin_channels=gin_channels, mean_only=True).to(device))
-            self.flows.append(Flip().to(device))
+            self.flows.append(ResidualCouplingLayer(channels, hidden_channels, kernel_size, dilation_rate, n_layers, gin_channels=gin_channels, mean_only=True))
+            self.flows.append(Flip())
 
     def forward(self, x, x_mask, g=None, reverse=False):
         flows = reversed(self.flows) if reverse else self.flows
@@ -60,9 +59,9 @@ class PosteriorEncoder(nn.Module):
     def __init__(self, in_channels, out_channels, hidden_channels, kernel_size, dilation_rate, n_layers, gin_channels=0):
         super().__init__()
         self.out_channels = out_channels
-        self.pre = nn.Conv1d(in_channels, hidden_channels, 1).to(device)
-        self.enc = WN(hidden_channels, kernel_size, dilation_rate, n_layers, gin_channels=gin_channels).to(device)
-        self.proj = nn.Conv1d(hidden_channels, out_channels * 2, 1).to(device)
+        self.pre = nn.Conv1d(in_channels, hidden_channels, 1)
+        self.enc = WN(hidden_channels, kernel_size, dilation_rate, n_layers, gin_channels=gin_channels)
+        self.proj = nn.Conv1d(hidden_channels, out_channels * 2, 1)
 
     def forward(self, x, x_lengths, g=None):
         x_mask = torch.unsqueeze(sequence_mask(x_lengths, x.size(2)), 1).to(x.dtype).to(device)
@@ -82,24 +81,24 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.num_kernels = len(resblock_kernel_sizes)
         self.num_upsamples = len(upsample_rates)
-        self.conv_pre = Conv1d(initial_channel, upsample_initial_channel, 7, 1, padding=3).to(device)
+        self.conv_pre = Conv1d(initial_channel, upsample_initial_channel, 7, 1, padding=3)
         resblock = ResBlock1 if resblock == "1" else ResBlock2
 
         self.ups = nn.ModuleList()
         for i, (u, k) in enumerate(zip(upsample_rates, upsample_kernel_sizes)):
-            self.ups.append(weight_norm(ConvTranspose1d(upsample_initial_channel // (2**i), upsample_initial_channel // (2 ** (i + 1)), k, u, padding=(k - u) // 2)).to(device))
+            self.ups.append(weight_norm(ConvTranspose1d(upsample_initial_channel // (2**i), upsample_initial_channel // (2 ** (i + 1)), k, u, padding=(k - u) // 2)))
 
         self.resblocks = nn.ModuleList()
         for i in range(len(self.ups)):
             ch = upsample_initial_channel // (2 ** (i + 1))
             for j, (k, d) in enumerate(zip(resblock_kernel_sizes, resblock_dilation_sizes)):
-                self.resblocks.append(resblock(ch, k, d).to(device))
+                self.resblocks.append(resblock(ch, k, d))
 
-        self.conv_post = Conv1d(ch, 1, 7, 1, padding=3, bias=False).to(device)
+        self.conv_post = Conv1d(ch, 1, 7, 1, padding=3, bias=False)
         self.ups.apply(init_weights)
 
         if gin_channels:
-            self.cond = nn.Conv1d(gin_channels, upsample_initial_channel, 1).to(device)
+            self.cond = nn.Conv1d(gin_channels, upsample_initial_channel, 1)
 
     def forward(self, x, g=None):
         x = self.conv_pre(x)
@@ -140,9 +139,7 @@ class SineGen(nn.Module):
     def _f02uv(self, f0):
         uv = torch.ones_like(f0)
         uv = uv * (f0 > self.voiced_threshold)
-        if uv.device.type == "privateuseone":
-            uv = uv.float()
-        return uv
+        return uv.float()
 
     def forward(self, f0, upp):
         with torch.no_grad():
@@ -177,8 +174,8 @@ class SourceModuleHnNSF(nn.Module):
         super(SourceModuleHnNSF, self).__init__()
 
         self.l_sin_gen = SineGen(sampling_rate, harmonic_num, sine_amp, add_noise_std, voiced_threshod)
-        self.l_linear = nn.Linear(harmonic_num + 1, 1).to(device)
-        self.l_tanh = nn.Tanh().to(device)
+        self.l_linear = nn.Linear(harmonic_num + 1, 1)
+        self.l_tanh = nn.Tanh()
 
     def forward(self, x, upp=None):
         if hasattr(self, "ddtype") == False:
@@ -195,32 +192,32 @@ class GeneratorNSF(nn.Module):
         super(GeneratorNSF, self).__init__()
         self.num_kernels = len(resblock_kernel_sizes)
         self.num_upsamples = len(upsample_rates)
-        self.f0_upsamp = nn.Upsample(scale_factor=np.prod(upsample_rates)).to(device)
-        self.m_source = SourceModuleHnNSF(sampling_rate=sr, harmonic_num=0, is_half=is_half).to(device)
+        self.f0_upsamp = nn.Upsample(scale_factor=np.prod(upsample_rates))
+        self.m_source = SourceModuleHnNSF(sampling_rate=sr, harmonic_num=0, is_half=is_half)
         self.noise_convs = nn.ModuleList()
-        self.conv_pre = Conv1d(initial_channel, upsample_initial_channel, 7, 1, padding=3).to(device)
+        self.conv_pre = Conv1d(initial_channel, upsample_initial_channel, 7, 1, padding=3)
         resblock = ResBlock1 if resblock == "1" else ResBlock2
         self.ups = nn.ModuleList()
         for i, (u, k) in enumerate(zip(upsample_rates, upsample_kernel_sizes)):
             c_cur = upsample_initial_channel // (2 ** (i + 1))
-            self.ups.append(weight_norm(ConvTranspose1d(upsample_initial_channel // (2**i), upsample_initial_channel // (2 ** (i + 1)), k, u, padding=(k - u) // 2)).to(device))
+            self.ups.append(weight_norm(ConvTranspose1d(upsample_initial_channel // (2**i), upsample_initial_channel // (2 ** (i + 1)), k, u, padding=(k - u) // 2)))
             if i + 1 < len(upsample_rates):
                 stride_f0 = np.prod(upsample_rates[i + 1 :])
-                self.noise_convs.append(Conv1d(1, c_cur, kernel_size=stride_f0 * 2, stride=stride_f0, padding=stride_f0 // 2).to(device))
+                self.noise_convs.append(Conv1d(1, c_cur, kernel_size=stride_f0 * 2, stride=stride_f0, padding=stride_f0 // 2))
             else:
-                self.noise_convs.append(Conv1d(1, c_cur, kernel_size=1).to(device))
+                self.noise_convs.append(Conv1d(1, c_cur, kernel_size=1))
 
         self.resblocks = nn.ModuleList()
         for i in range(len(self.ups)):
             ch = upsample_initial_channel // (2 ** (i + 1))
             for j, (k, d) in enumerate(zip(resblock_kernel_sizes, resblock_dilation_sizes)):
-                self.resblocks.append(resblock(ch, k, d).to(device))
+                self.resblocks.append(resblock(ch, k, d))
 
-        self.conv_post = Conv1d(ch, 1, 7, 1, padding=3, bias=False).to(device)
+        self.conv_post = Conv1d(ch, 1, 7, 1, padding=3, bias=False)
         self.ups.apply(init_weights)
 
         if gin_channels:
-            self.cond = nn.Conv1d(gin_channels, upsample_initial_channel, 1).to(device)
+            self.cond = nn.Conv1d(gin_channels, upsample_initial_channel, 1)
 
         self.upp = np.prod(upsample_rates)
 
@@ -266,11 +263,11 @@ class Synthesizer(nn.Module):
         super().__init__()
         sr = sr2sr[sr] if isinstance(sr, str) else sr
         self.segment_size = segment_size
-        self.enc_p = TextEncoder(input_dim, inter_channels, hidden_channels, filter_channels, n_heads, n_layers, kernel_size, p_dropout).to(device)
-        self.dec = GeneratorNSF(inter_channels, resblock, resblock_kernel_sizes, resblock_dilation_sizes, upsample_rates, upsample_initial_channel, upsample_kernel_sizes, gin_channels=gin_channels, sr=sr, is_half=kwargs["is_half"]).to(device)
-        self.enc_q = PosteriorEncoder(spec_channels, inter_channels, hidden_channels, 5, 1, 16, gin_channels=gin_channels).to(device)
-        self.flow = ResidualCouplingBlock(inter_channels, hidden_channels, 5, 1, 3, gin_channels=gin_channels).to(device)
-        self.emb_g = nn.Embedding(spk_embed_dim, gin_channels).to(device)
+        self.enc_p = TextEncoder(input_dim, inter_channels, hidden_channels, filter_channels, n_heads, n_layers, kernel_size, p_dropout)
+        self.dec = GeneratorNSF(inter_channels, resblock, resblock_kernel_sizes, resblock_dilation_sizes, upsample_rates, upsample_initial_channel, upsample_kernel_sizes, gin_channels=gin_channels, sr=sr, is_half=kwargs["is_half"])
+        self.enc_q = PosteriorEncoder(spec_channels, inter_channels, hidden_channels, 5, 1, 16, gin_channels=gin_channels)
+        self.flow = ResidualCouplingBlock(inter_channels, hidden_channels, 5, 1, 3, gin_channels=gin_channels)
+        self.emb_g = nn.Embedding(spk_embed_dim, gin_channels)
 
     def remove_weight_norm(self):
         self.dec.remove_weight_norm()
@@ -305,11 +302,11 @@ class Synthesizer_nono(nn.Module):
     def __init__(self, input_dim, spec_channels, segment_size, inter_channels, hidden_channels, filter_channels, n_heads, n_layers, kernel_size, p_dropout, resblock, resblock_kernel_sizes, resblock_dilation_sizes, upsample_rates, upsample_initial_channel, upsample_kernel_sizes, spk_embed_dim, gin_channels, sr=None, **kwargs):
         super().__init__()
         self.segment_size = segment_size
-        self.enc_p = TextEncoder(input_dim, inter_channels, hidden_channels, filter_channels, n_heads, n_layers, kernel_size, p_dropout, f0=False).to(device)
-        self.dec = Generator(inter_channels, resblock, resblock_kernel_sizes, resblock_dilation_sizes, upsample_rates, upsample_initial_channel, upsample_kernel_sizes, gin_channels=gin_channels).to(device)
-        self.enc_q = PosteriorEncoder(spec_channels, inter_channels, hidden_channels, 5, 1, 16, gin_channels=gin_channels).to(device)
-        self.flow = ResidualCouplingBlock(inter_channels, hidden_channels, 5, 1, 3, gin_channels=gin_channels).to(device)
-        self.emb_g = nn.Embedding(spk_embed_dim, gin_channels).to(device)
+        self.enc_p = TextEncoder(input_dim, inter_channels, hidden_channels, filter_channels, n_heads, n_layers, kernel_size, p_dropout, f0=False)
+        self.dec = Generator(inter_channels, resblock, resblock_kernel_sizes, resblock_dilation_sizes, upsample_rates, upsample_initial_channel, upsample_kernel_sizes, gin_channels=gin_channels)
+        self.enc_q = PosteriorEncoder(spec_channels, inter_channels, hidden_channels, 5, 1, 16, gin_channels=gin_channels)
+        self.flow = ResidualCouplingBlock(inter_channels, hidden_channels, 5, 1, 3, gin_channels=gin_channels)
+        self.emb_g = nn.Embedding(spk_embed_dim, gin_channels)
 
     def remove_weight_norm(self):
         self.dec.remove_weight_norm()
@@ -343,8 +340,8 @@ class MultiPeriodDiscriminator(nn.Module):
         super(MultiPeriodDiscriminator, self).__init__()
         periods = [2, 3, 5, 7, 11, 17]
 
-        discs = [DiscriminatorS(use_spectral_norm=use_spectral_norm).to(device)]
-        discs = discs + [DiscriminatorP(i, use_spectral_norm=use_spectral_norm).to(device) for i in periods]
+        discs = [DiscriminatorS(use_spectral_norm=use_spectral_norm)]
+        discs += [DiscriminatorP(i, use_spectral_norm=use_spectral_norm) for i in periods]
         self.discriminators = nn.ModuleList(discs)
 
     def forward(self, y, y_hat):
@@ -364,8 +361,8 @@ class MultiPeriodDiscriminatorV2(nn.Module):
         super(MultiPeriodDiscriminatorV2, self).__init__()
         periods = [2, 3, 5, 7, 11, 17, 23, 37]
 
-        discs = [DiscriminatorS(use_spectral_norm=use_spectral_norm).to(device)]
-        discs = discs + [DiscriminatorP(i, use_spectral_norm=use_spectral_norm).to(device) for i in periods]
+        discs = [DiscriminatorS(use_spectral_norm=use_spectral_norm)]
+        discs += [DiscriminatorP(i, use_spectral_norm=use_spectral_norm) for i in periods]
         self.discriminators = nn.ModuleList(discs)
 
     def forward(self, y, y_hat):
@@ -385,14 +382,14 @@ class DiscriminatorS(nn.Module):
         super(DiscriminatorS, self).__init__()
         norm_f = weight_norm if not use_spectral_norm else spectral_norm
         self.convs = nn.ModuleList([
-            norm_f(Conv1d(1, 16, 15, 1, padding=7).to(device)),
-            norm_f(Conv1d(16, 64, 41, 4, groups=4, padding=20).to(device)),
-            norm_f(Conv1d(64, 256, 41, 4, groups=16, padding=20).to(device)),
-            norm_f(Conv1d(256, 1024, 41, 4, groups=64, padding=20).to(device)),
-            norm_f(Conv1d(1024, 1024, 41, 4, groups=256, padding=20).to(device)),
-            norm_f(Conv1d(1024, 1024, 5, 1, padding=2).to(device))
+            norm_f(Conv1d(1, 16, 15, 1, padding=7)),
+            norm_f(Conv1d(16, 64, 41, 4, groups=4, padding=20)),
+            norm_f(Conv1d(64, 256, 41, 4, groups=16, padding=20)),
+            norm_f(Conv1d(256, 1024, 41, 4, groups=64, padding=20)),
+            norm_f(Conv1d(1024, 1024, 41, 4, groups=256, padding=20)),
+            norm_f(Conv1d(1024, 1024, 5, 1, padding=2))
         ])
-        self.conv_post = norm_f(Conv1d(1024, 1, 3, 1, padding=1).to(device))
+        self.conv_post = norm_f(Conv1d(1024, 1, 3, 1, padding=1))
 
     def forward(self, x):
         fmap = []
@@ -411,13 +408,13 @@ class DiscriminatorP(nn.Module):
         self.period = period
         norm_f = weight_norm if not use_spectral_norm else spectral_norm
         self.convs = nn.ModuleList([
-            norm_f(Conv2d(1, 32, (kernel_size, 1), (stride, 1), padding=(get_padding(kernel_size, 1), 0)).to(device)),
-            norm_f(Conv2d(32, 128, (kernel_size, 1), (stride, 1), padding=(get_padding(kernel_size, 1), 0)).to(device)),
-            norm_f(Conv2d(128, 512, (kernel_size, 1), (stride, 1), padding=(get_padding(kernel_size, 1), 0)).to(device)),
-            norm_f(Conv2d(512, 1024, (kernel_size, 1), (stride, 1), padding=(get_padding(kernel_size, 1), 0)).to(device)),
-            norm_f(Conv2d(1024, 1024, (kernel_size, 1), 1, padding=(get_padding(kernel_size, 1), 0)).to(device))
+            norm_f(Conv2d(1, 32, (kernel_size, 1), (stride, 1), padding=(get_padding(kernel_size, 1), 0))),
+            norm_f(Conv2d(32, 128, (kernel_size, 1), (stride, 1), padding=(get_padding(kernel_size, 1), 0))),
+            norm_f(Conv2d(128, 512, (kernel_size, 1), (stride, 1), padding=(get_padding(kernel_size, 1), 0))),
+            norm_f(Conv2d(512, 1024, (kernel_size, 1), (stride, 1), padding=(get_padding(kernel_size, 1), 0))),
+            norm_f(Conv2d(1024, 1024, (kernel_size, 1), 1, padding=(get_padding(kernel_size, 1), 0)))
         ])
-        self.conv_post = norm_f(Conv2d(1024, 1, (3, 1), 1, padding=(1, 0)).to(device))
+        self.conv_post = norm_f(Conv2d(1024, 1, (3, 1), 1, padding=(1, 0)))
 
     def forward(self, x):
         fmap = []
