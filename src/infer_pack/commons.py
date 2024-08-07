eb579c5c0234f05aa1c3d729f133d85485330e2e
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-08-07 13:58:36 +0500

    Update commons.py

--- src/infer_pack/commons.py
+++ src/infer_pack/commons.py
@@ -4,7 +4,7 @@ from torch import nn
 from torch.nn import functional as F
 
 def init_weights(m, mean=0.0, std=0.01):
-    if isinstance(m, nn.Conv2d) or isinstance(m, nn.Conv1d):
+    if isinstance(m, (nn.Conv2d, nn.Conv1d)):
         m.weight.data.normal_(mean, std)
 
 def get_padding(kernel_size, dilation=1):
@@ -15,7 +15,7 @@ def convert_pad_shape(pad_shape):
 
 def kl_divergence(m_p, logs_p, m_q, logs_q):
     kl = (logs_q - logs_p) - 0.5
-    kl += (0.5 * (torch.exp(2.0 * logs_p) + ((m_p - m_q) ** 2)) * torch.exp(-2.0 * logs_q))
+    kl += 0.5 * (torch.exp(2.0 * logs_p) + (m_p - m_q).pow(2)) * torch.exp(-2.0 * logs_q)
     return kl
 
 def rand_gumbel(shape):
@@ -42,19 +42,18 @@ def rand_slice_segments(x, x_lengths=None, segment_size=4):
     if x_lengths is None:
         x_lengths = t
     ids_str_max = x_lengths - segment_size + 1
-    ids_str = (torch.rand([b]).to(device=x.device) * ids_str_max).to(dtype=torch.long)
+    ids_str = (torch.rand([b]).to(device=x.device) * ids_str_max).long()
     ret = slice_segments(x, ids_str, segment_size)
     return ret, ids_str
 
 def get_timing_signal_1d(length, channels, min_timescale=1.0, max_timescale=1.0e4):
     position = torch.arange(length, dtype=torch.float)
     num_timescales = channels // 2
-    log_timescale_increment = math.log(float(max_timescale) / float(min_timescale)) / (num_timescales - 1)
+    log_timescale_increment = math.log(max_timescale / min_timescale) / (num_timescales - 1)
     inv_timescales = min_timescale * torch.exp(torch.arange(num_timescales, dtype=torch.float) * -log_timescale_increment)
     scaled_time = position.unsqueeze(0) * inv_timescales.unsqueeze(1)
     signal = torch.cat([torch.sin(scaled_time), torch.cos(scaled_time)], 0)
-    signal = F.pad(signal, [0, 0, 0, channels % 2])
-    signal = signal.view(1, channels, length)
+    signal = F.pad(signal, [0, 0, 0, channels % 2]).view(1, channels, length)
     return signal
 
 def add_timing_signal_1d(x, min_timescale=1.0, max_timescale=1.0e4):
@@ -76,8 +75,7 @@ def fused_add_tanh_sigmoid_multiply(input_a, input_b, n_channels):
     in_act = input_a + input_b
     t_act = torch.tanh(in_act[:, :n_channels_int, :])
     s_act = torch.sigmoid(in_act[:, n_channels_int:, :])
-    acts = t_act * s_act
-    return acts
+    return t_act * s_act
 
 def shift_1d(x):
     return F.pad(x, convert_pad_shape([[0, 0], [0, 0], [1, 0]]))[:, :, :-1]
@@ -90,13 +88,10 @@ def sequence_mask(length, max_length=None):
 
 def generate_path(duration, mask):
     device = duration.device
-
     b, _, t_y, t_x = mask.shape
     cum_duration = torch.cumsum(duration, -1)
-
     cum_duration_flat = cum_duration.view(b * t_x)
-    path = sequence_mask(cum_duration_flat, t_y).to(mask.dtype)
-    path = path.view(b, t_x, t_y)
+    path = sequence_mask(cum_duration_flat, t_y).to(mask.dtype).view(b, t_x, t_y)
     path = path - F.pad(path, convert_pad_shape([[0, 0], [1, 0], [0, 0]]))[:, :-1]
     path = path.unsqueeze(1).transpose(2, 3) * mask
     return path
@@ -108,12 +103,10 @@ def clip_grad_value_(parameters, clip_value, norm_type=2):
     norm_type = float(norm_type)
     if clip_value is not None:
         clip_value = float(clip_value)
-
     total_norm = 0
     for p in parameters:
         param_norm = p.grad.data.norm(norm_type)
         total_norm += param_norm.item() ** norm_type
         if clip_value is not None:
             p.grad.data.clamp_(min=-clip_value, max=clip_value)
-    total_norm = total_norm ** (1.0 / norm_type)
-    return total_norm
+    return total_norm ** (1.0 / norm_type)
