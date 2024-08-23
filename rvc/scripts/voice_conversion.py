a330ca5cc5ddb05d39afe010fd7ffdb453067f5b
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-08-23 21:31:29 +0500

    Update voice_conversion.py

--- rvc/scripts/voice_conversion.py
+++ rvc/scripts/voice_conversion.py
@@ -4,7 +4,6 @@ import shlex
 import subprocess
 import librosa
 import torch
-import numpy as np
 import gradio as gr
 
 from rvc.infer.infer import Config, load_hubert, get_vc, rvc_infer
@@ -13,8 +12,7 @@ RVC_MODELS_DIR = os.path.join(os.getcwd(), 'models', 'rvc_models')
 HUBERT_MODEL_PATH = os.path.join(os.getcwd(), 'models', 'assets', 'hubert_base.pt')
 OUTPUT_DIR = os.path.join(os.getcwd(), 'output')
 
-if not os.path.exists(OUTPUT_DIR):
-    os.makedirs(OUTPUT_DIR)
+os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 def display_progress(percent, message, progress=gr.Progress()):
     progress(percent, desc=message)
@@ -26,7 +24,7 @@ def load_rvc_model(voice_model):
     rvc_index_path = next((os.path.join(model_dir, f) for f in model_files if f.endswith('.index')), None)
 
     if not rvc_model_path:
-        raise ValueError(f'\033[91mМодели {voice_model} не существует. Возможно, вы неправильно ввели имя.\033[0m')
+        raise ValueError(f'Модели {voice_model} не существует. Возможно, вы неправильно ввели имя.')
 
     return rvc_model_path, rvc_index_path
 
@@ -39,19 +37,24 @@ def convert_audio_to_stereo(audio_path):
     return audio_path
 
 def perform_voice_conversion(
-    voice_model, vocals_path, output_path, pitch, f0_method, index_rate, filter_radius, volume_envelope, protect, hop_length, f0_min, f0_max, device_type
+    voice_model,
+    vocals_path,
+    output_path,
+    pitch,
+    f0_method,
+    index_rate,
+    filter_radius,
+    volume_envelope,
+    protect,
+    hop_length,
+    f0_min,
+    f0_max,
 ):
     rvc_model_path, rvc_index_path = load_rvc_model(voice_model)
-    device = torch.device(device_type)
+    config = Config()
 
-    if device_type == 'cuda' and not torch.cuda.is_available():
-        print("GPU недоступен. Автоматически выбран CPU.")
-        gr.Error("GPU недоступен. Автоматически выбран CPU.")
-        device = torch.device('cpu')
-
-    config = Config(device, True)
-    hubert_model = load_hubert(device, config.is_half, HUBERT_MODEL_PATH)
-    cpt, version, net_g, tgt_sr, vc = get_vc(device, config.is_half, config, rvc_model_path)
+    hubert_model = load_hubert(config.device, config.is_half, HUBERT_MODEL_PATH)
+    cpt, version, net_g, tgt_sr, vc = get_vc(config.device, config.is_half, config, rvc_model_path)
 
     rvc_infer(
         rvc_index_path, index_rate, vocals_path, output_path, pitch, f0_method, cpt, version, net_g,
@@ -63,16 +66,26 @@ def perform_voice_conversion(
     torch.cuda.empty_cache()
 
 def voice_pipeline(
-    uploaded_file, voice_model, pitch, device_type, index_rate=0.5, filter_radius=3, volume_envelope=0.25,
-    f0_method='rmvpe+', hop_length=128, protect=0.33, output_format='mp3', f0_min=50, f0_max=1100,
-    progress=gr.Progress()
+    uploaded_file,
+    voice_model,
+    pitch,
+    index_rate=0.5,
+    filter_radius=3,
+    volume_envelope=0.25,
+    f0_method='rmvpe+',
+    hop_length=128,
+    protect=0.33,
+    output_format='mp3',
+    f0_min=50,
+    f0_max=1100,
+    progress=gr.Progress(),
 ):
     if not uploaded_file:
         raise ValueError("Не удалось найти аудиофайл. Убедитесь, что файл загрузился или проверьте правильность пути к нему.")
     if not voice_model:
         raise ValueError("Выберите модель голоса для преобразования.")
 
-    display_progress(0, '[~] Запуск конвейера генерации AI-кавера...', progress)
+    display_progress(0, 'Запуск конвейера генерации AI-кавера...', progress)
 
     if not os.path.exists(uploaded_file):
         raise ValueError(f'Файл {uploaded_file} не найден.')
@@ -83,10 +96,10 @@ def voice_pipeline(
     if os.path.exists(voice_convert_path):
         os.remove(voice_convert_path)
 
-    display_progress(0.5, '[~] Преобразование вокала...', progress)
+    display_progress(0.5, 'Преобразование вокала...', progress)
     perform_voice_conversion(
         voice_model, orig_song_path, voice_convert_path, pitch, f0_method, index_rate,
-        filter_radius, volume_envelope, protect, hop_length, f0_min, f0_max, device_type
+        filter_radius, volume_envelope, protect, hop_length, f0_min, f0_max
     )
 
     return voice_convert_path
