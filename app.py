b168d3dfbb30aab82d2f0aeece6ab92a2006734f
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-08-25 17:13:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gradio as gr
 import sys
 import os
 
-#    from tabs.welcome import *
+from tabs.welcome import *
 from tabs.conversion.conversion import *
 from tabs.conversion.edge_tts import *
 from tabs.processing.processing import *
@@ -20,8 +20,8 @@ with gr.Blocks(
     )
 ) as PolGen:
         
-#    with gr.Tab("Велком/Контакты"):
-#        welcome_tab()
+    with gr.Tab("Велком/Контакты"):
+        welcome_tab()
 
     with gr.Tab("Преобразование и обработка голоса"):
         with gr.Tab("Замена голоса"):
@@ -38,34 +38,4 @@ with gr.Blocks(
         zip_upload()
         files_upload()
 
-
-def launch_gradio(port):
-    PolGen.launch(
-        share="--share" in sys.argv,
-        inbrowser="--open" in sys.argv,
-        quiet="--quiet" in sys.argv,
-        server_port=port,
-        show_error=True,
-        show_api=False,
-    )
-
-if __name__ == "__main__":
-    port = 6666
-    if "--port" in sys.argv:
-        port_index = sys.argv.index("--port") + 1
-        if port_index < len(sys.argv):
-            port = int(sys.argv[port_index])
-
-        launch_gradio(port)
-
-    else:
-        for i in range(10):
-            try:
-                launch_gradio(port)
-                break
-            except OSError:
-                print("Не удалось запустить на порту", port, "- пытаюсь снова...")
-                port -= 1
-            except Exception as error:
-                print(f"Произошла ошибка при запуске Gradio: {error}")
-                break
+app.launch(share=True, show_error=True, quiet=True, show_api=False)
