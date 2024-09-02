6186f3953ad5220cfc70f00219ed8e44b307f55e
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-09-02 16:36:58 +0500

    Update welcome.py

--- tabs/welcome.py
+++ tabs/welcome.py
@@ -1,5 +1,6 @@
 import gradio as gr
 
+
 def welcome_tab():
     gr.HTML("<center><h1>Добро пожаловать в PolGen Lite - Politrees (v1.2)</h1></center>")
     with gr.Row():
@@ -12,5 +13,3 @@ def welcome_tab():
     with gr.Column(variant='panel'):
         gr.HTML("<center><h2><a href='https://www.youtube.com/channel/UCHb3fZEVxUisnqLqCrEM8ZA'>YouTube</a></h2></center>")
         gr.HTML("<center><h2><a href='https://github.com/Bebra777228/Pol-Litres-RVC'>GitHub</a></h2></center>")
-    with gr.Column(variant='panel'):
-        gr.HTML("<center><h2>Спасибо <a href='https://t.me/Player1444'>Player1444</a> за помощь в тестах.</h2></center>")
